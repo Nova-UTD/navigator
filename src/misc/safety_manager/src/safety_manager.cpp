84b53351e6d8f74d403b36fb7203712d36bdb5d8
Joshua Williams <joshmackwilliams@protonmail.com>
2021-11-04 17:51:14 -0500

    Implement hello-world package safety_manager

---
+++ src/misc/safety_manager/src/safety_manager.cpp
@@ -0,0 +1,17 @@
+/*
+ * Package:   safety_manager
+ * Filename:  src/safety_manager.cpp
+ * Author:    Joshua Williams
+ * Email:     joshmackwilliams@protonmail.com
+ * Copyright: 2021, Nova UTD
+ * License:   MIT License
+ */
+
+// The safety manager. Currently just a hello world, but that's going to change.
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello world!" << std::endl;
+  return 0;
+}
---
+++ src/misc/safety_manager/test/hello_world.cpp
@@ -0,0 +1,7 @@
+// Just a dummy test file, for now
+
+#include <gtest/gtest.h>
+
+TEST(TestHelloWorld, test_hello_world) {
+  ASSERT_TRUE(true);
+}
