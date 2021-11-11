92fda9391f192206fd2f76182845176ef4b942a8
Raghav <raghavpillai101@gmail.com>
2021-11-10 20:29:34 -0600

    Changed some comments

--- src/external/ros2-webbridge/index.js
+++ src/external/ros2-webbridge/index.js
@@ -1,17 +1,3 @@
-// Copyright (c) 2017 Intel Corporation. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 'use strict';
 
 const rclnodejs = require('rclnodejs');
@@ -134,7 +120,7 @@ function createConnection(options) {
   });
 
   let wsAddr = options.address || `ws://localhost:${options.port}`;
-  console.log(`Websocket started on ${wsAddr}`);
+  console.log(`Nova websocket started on ${wsAddr}`);
   // gracefuly shutdown rosbridge node commanding terminal
   process.on('SIGINT', () => process.exit(1));
 }
