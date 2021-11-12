141401ed3d56f9e2ae569fc3afc8f108e39c6f26
Joshua Williams <joshmackwilliams@protonmail.com>
2021-11-12 14:12:22 -0600

    safety manager now logs some info about events

--- src/misc/safety_manager/src/SafetyManagerNode.cpp
+++ src/misc/safety_manager/src/SafetyManagerNode.cpp
@@ -9,6 +9,8 @@
 
 #include "safety_manager/SafetyManagerNode.hpp"
 
+#include <string>
+
 using namespace Nova::SafetyManager;
 using voltron_msgs::srv::SafetyEvent;
 
@@ -25,5 +27,23 @@ SafetyManagerNode::~SafetyManagerNode() {
 }
 
 void SafetyManagerNode::log_event(const SafetyEvent::Request::SharedPtr & request, SafetyEvent::Response::SharedPtr & response) {
-  RCLCPP_INFO(this->get_logger(), "Got a safety event!");
+  std::string message;
+  switch(request->status) {
+  case SafetyEvent::Request::STATUS_WORKING:
+    message += "A node is working to resolve safety event: ";
+    break;
+  case SafetyEvent::Request::STATUS_UNRESOLVED:
+    message += "A safety event is unresolved: ";
+    break;
+  case SafetyEvent::Request::STATUS_RESOLVED:
+    message += "A safety event has been resolved: ";
+    break;
+  default:
+    message += "A safety event was raised with a bad status: ";
+    break;
+  }
+  message += request->description;
+  RCLCPP_INFO(this->get_logger(), message);
+
+  (void) response;
 }
