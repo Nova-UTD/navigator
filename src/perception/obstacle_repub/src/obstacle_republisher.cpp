0c13acbfa9ea8d09b8919db622805dea1c89c7cb
Ragib Arnab <rae3840924@gmail.com>
2022-01-05 21:23:33 -0600

    began working with ZED messages

--- src/perception/obstacle_repub/src/obstacle_republisher.cpp
+++ src/perception/obstacle_repub/src/obstacle_republisher.cpp
@@ -9,6 +9,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "lgsvl_msgs/msg/detection3_d_array.hpp"
+#include "zed_interfaces/msg/objects_stamped.hpp"
 #include "voltron_msgs/msg/obstacle3_d.hpp"
 #include "voltron_msgs/msg/obstacle3_d_array.hpp"
 
@@ -20,10 +21,15 @@ class ObstacleRepublisher : public rclcpp::Node {
 
 public:
     ObstacleRepublisher() : Node("obstacle_republisher"){
-        // creates a subscription that listens to the topic published from SVL's 3D Ground Truth sensor
+        // creates a subscription that listens to the object detection topic published from SVL's 3D Ground Truth sensor
         // and then calls back to a private method in this class definition
         svl_obstacle_subscription = this->create_subscription<lgsvl_msgs::msg::Detection3DArray>(
         "/ground_truth_3d/detections", 10, std::bind(&ObstacleRepublisher::svl_obstacle_callback, this, _1));
+
+        // creates a subscription that listens to the object detection topic published from ZED Ros2 Node 
+        // and then calls back to a private method in this class definition
+        zed_obstacle_subscription = this->create_subscription<zed_interfaces::msg::ObjectsStamped>(
+        "/obj_det/objects", 10, std::bind(&ObstacleRepublisher::zed_obstacle_callback, this, _1));
 
         // an publishers that republishes 3D vehicle detections into our own abstract format
         std_vehicle_publisher = this->create_publisher<voltron_msgs::msg::Obstacle3DArray>("/obstacles/vehicles", 10);
@@ -34,6 +40,7 @@ public:
 private:
 
     rclcpp::Subscription<lgsvl_msgs::msg::Detection3DArray>::SharedPtr svl_obstacle_subscription;
+    rclcpp::Subscription<zed_interfaces::msg::ObjectsStamped>::SharedPtr zed_obstacle_subscription;
     rclcpp::Publisher<voltron_msgs::msg::Obstacle3DArray>::SharedPtr std_vehicle_publisher;
     rclcpp::Publisher<voltron_msgs::msg::Obstacle3DArray>::SharedPtr std_pedestrian_publisher;
 
@@ -61,6 +68,48 @@ private:
             }
         }
 
+        std_vehicle_publisher->publish(std_vehicle_array);
+        std_pedestrian_publisher->publish(std_pedestrian_array);
+    }
+
+    void zed_obstacle_callback(const zed_interfaces::msg::ObjectsStamped::SharedPtr msg) const {
+        auto std_vehicle_array = voltron_msgs::msg::Obstacle3DArray();
+        auto std_pedestrian_array = voltron_msgs::msg::Obstacle3DArray();
+        
+        for (const auto detection : msg->objects) {
+            auto std_obstacle = voltron_msgs::msg::Obstacle3D();
+
+            std_obstacle.id = detection.label_id;
+            
+            std_obstacle.confidence = detection.confidence / 100.0f;
+
+            std_obstacle.velocity.set__x(static_cast<double>(detection.velocity.at(0)));
+            std_obstacle.velocity.set__y(static_cast<double>(detection.velocity.at(1)));
+            std_obstacle.velocity.set__z(static_cast<double>(detection.velocity.at(2)));
+
+            std_obstacle.bounding_box.box_size.set__x(static_cast<double>(detection.dimensions_3d.at(0)));
+            std_obstacle.bounding_box.box_size.set__y(static_cast<double>(detection.dimensions_3d.at(1)));
+            std_obstacle.bounding_box.box_size.set__z(static_cast<double>(detection.dimensions_3d.at(2)));
+
+            std_obstacle.bounding_box.box_pose.position.set__x(static_cast<double>(detection.position.at(0)));
+            std_obstacle.bounding_box.box_pose.position.set__y(static_cast<double>(detection.position.at(1)));
+            std_obstacle.bounding_box.box_pose.position.set__z(static_cast<double>(detection.position.at(2)));
+
+            // TO-DO: find the orientation of the bounding box object, requires some knowledge of quaternions
+            // std_obstacle.bounding_box.box_pose.orientation 
+
+
+            if (detection.label == "Vehicle"){
+                std_obstacle.label = "Vehicle";
+                std_vehicle_array.obstacles.push_back(std_obstacle);
+            }
+            else if (detection.label == "Person"){
+                std_obstacle.label = "Pedestrian";
+                std_pedestrian_array.obstacles.push_back(std_obstacle);
+            }
+
+        }
+        
         std_vehicle_publisher->publish(std_vehicle_array);
         std_pedestrian_publisher->publish(std_pedestrian_array);
     }
