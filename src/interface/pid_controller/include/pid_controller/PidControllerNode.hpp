e46f30b6ccba816f7bf09a16bbbb94ad990e2b25
Joshua Williams <joshmackwilliams@protonmail.com>
2022-02-09 20:54:24 -0600

    Convert pid_controller to use floats

--- src/interface/pid_controller/include/pid_controller/PidControllerNode.hpp
+++ src/interface/pid_controller/include/pid_controller/PidControllerNode.hpp
@@ -14,7 +14,6 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float32.hpp"
-#include "autoware_auto_msgs/msg/vehicle_control_command.hpp"
 
 #include "pid_controller/PidController.hpp"
 
@@ -29,7 +28,7 @@ public:
   virtual ~PidControllerNode();
 
 private:
-  void update_target(const autoware_auto_msgs::msg::VehicleControlCommand::SharedPtr command);
+  void update_target(const std_msgs::msg::Float32::SharedPtr command);
   void update_measurement(const std_msgs::msg::Float32::SharedPtr measurement);
   void recalculate_output();
 
@@ -40,7 +39,7 @@ private:
   clock_t::time_point last_update_time;
 
   rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr steering_control_publisher;
-  rclcpp::Subscription<autoware_auto_msgs::msg::VehicleControlCommand>::
+  rclcpp::Subscription<std_msgs::msg::Float32>::
     SharedPtr command_subscription;
   rclcpp::Subscription<std_msgs::msg::Float32>::
     SharedPtr measurement_subscription;
