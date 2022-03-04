3ab00e8077e49f7e7f769a284f63c829ef67344c
Joshua Williams <joshmackwilliams@protonmail.com>
2022-03-04 13:58:59 -0600

    Add boost package and start on opendrive_utils

---
+++ src/common/opendrive_utils/include/opendrive_utils/GeometricMap.hpp
@@ -0,0 +1,24 @@
+/*
+ * Package:   opendrive_utils
+ * Filename:  GeometricMap.hpp
+ * Author:    Joshua Williams
+ * Email:     joshmackwilliams@protonmail.com
+ * Copyright: 2022, Nova UTD
+ * License:   MIT License
+ */
+
+#pragma once
+#include "OpenDriveMap.h"
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point.hpp>
+#include <boost/geometry/geometries/box.hpp>
+
+namespace navigator {
+  class GeometricMap final {
+  public:
+    GeometricMap(const odr::OpenDriveMap & map);
+
+  private:
+    unsigned int number_of_roads;
+  }
+}
