d1cdf23f64507029265179543ba7be81738b8ca6
Will Heitman <will.heitman@utdallas.edu>
2022-03-17 11:04:32 -0500

    Add path publisher package

---
+++ src/planning/path_publisher/include/path_publisher/PathPublisherNode.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+#include <rclcpp/rclcpp.hpp>
+
+// libOpenDRIVE stuff
+#include "OpenDriveMap.h"
+#include "Lanes.h"
+#include "Road.h"
+
+// Message headers
+#include <geometry_msgs/msg/point.hpp>
+#include <geometry_msgs/msg/vector3.hpp>
+#include <std_msgs/msg/color_rgba.hpp>
+#include <visualization_msgs/msg/marker.hpp>
+#include <visualization_msgs/msg/marker_array.hpp>
+
+class PathPublisherNode : public rclcpp::Node {
+public:
+	PathPublisherNode();
+	void publishMarkerArray();
+
+private:
+	visualization_msgs::msg::MarkerArray lane_markers;
+	int point_count;
+    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr marker_pub;
+    rclcpp::TimerBase::SharedPtr map_pub_timer{nullptr};
+};
