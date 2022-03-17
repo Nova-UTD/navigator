a3a5e73dc0c35333fd0a5bb892ab5e8de27cb8fb
jim-works <jim4m80@gmail.com>
2022-03-17 17:25:47 -0500

    switch steering angle message

--- src/planning/motion_planner/include/motion_planner/MotionPlannerNode.hpp
+++ src/planning/motion_planner/include/motion_planner/MotionPlannerNode.hpp
@@ -31,6 +31,7 @@
 #include "voltron_msgs/msg/trajectory.hpp"
 #include "voltron_msgs/msg/trajectories.hpp"
 #include "voltron_msgs/msg/final_path.hpp"
+#include "voltron_msgs/msg/steering_position.hpp"
 #include "std_msgs/msg/float32.hpp"
 
 #include "motion_planner/MotionPlanner.hpp"
@@ -64,7 +65,7 @@ private:
     //helper function for current_pose_cb
     bool transform_pose_to_map(const geometry_msgs::msg::PoseStamped &pose_in,
           geometry_msgs::msg::PoseStamped &pose_out);
-    void update_steering_angle(std_msgs::msg::Float32::SharedPtr ptr);
+    void update_steering_angle(voltron_msgs::msg::SteeringPosition::SharedPtr ptr);
 
     rclcpp::Publisher<voltron_msgs::msg::Trajectory>::SharedPtr trajectory_publisher;
     rclcpp::Publisher<voltron_msgs::msg::Trajectories>::SharedPtr trajectory_viz_publisher;
@@ -72,7 +73,7 @@ private:
     rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr perception_subscription;
     rclcpp::Subscription<VehicleKinematicState>::SharedPtr current_pose_subscription;
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odomtery_pose_subscription;
-    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr steering_angle_subscription; //radians
+    rclcpp::Subscription<voltron_msgs::msg::SteeringPosition>::SharedPtr steering_angle_subscription; //radians
     rclcpp::TimerBase::SharedPtr control_timer;
 
     std::shared_ptr<MotionPlanner> planner;
