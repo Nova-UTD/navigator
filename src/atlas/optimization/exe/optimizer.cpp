7ed10150f7d5cf93da687b6bbde89bf85e4c58ed
Will Heitman <will.heitman@utdallas.edu>
2022-04-03 12:55:31 -0500

    Create Optimizer node for state estimation

---
+++ src/atlas/optimization/exe/optimizer.cpp
@@ -0,0 +1,9 @@
+#include "optimization/OptimizationNode.hpp"
+
+int main(int argc, char *argv[])
+{
+	rclcpp::init(argc, argv);
+	rclcpp::spin(std::make_shared<OptimizationNode>());
+	rclcpp::shutdown();
+	return 0;
+}
