428e0b03199dfbd75bb8dc975799ab53442e9e9e
Joshua Williams <joshmackwilliams@protonmail.com>
2022-04-23 18:16:54 -0500

    Progress on demo 2 launch file

--- launch/demo2.launch.py
+++ launch/demo2.launch.py
@@ -81,18 +81,119 @@ def generate_launch_description():
             ("target", "steering_target"),
             ("measurement", "real_steering_angle")])
 
-    gps = Node(
+    gnss_parser = Node(
         package = 'gnss_parser',
         executable = 'gnss_parser_node_exe',
         remappings = [
-            
+            ("/sensors/gnss/odom", "gnss_odom"),
+            ("/serial/gnss", "serial_incoming_lines")])
+
+    
+    vehicle_can = Node(
+        package = 'can_interface',
+        executable = 'interface',
+        remappings = [
+            ('can_interface_incoming_can_frames', 'vehicle_incoming_can'),
+            ('can_interface_outgoing_can_frames', 'vehicle_outgoing_can')],
+        arguments = ['can1'])
+
+    
+    speedometer_reporter = Node(
+        package='can_translation',
+        executable='float_reporter',
+        parameters=[(path.join
+                     (param_dir,"interface","speedometer_reporter.param.yaml"))],
+        remappings=[
+            ("incoming_can_frames", "vehicle_incoming_can"),
+            ("result_topic", "vehicle_speedometer")])
+
+    speedometer_translator = Node(
+        package='msg_translation',
+        executable='velocity_to_twist',
+        remappings=[
+            ("velocity_topic", "vehicle_speedometer"),
+            ("twist_topic", "speedometer_odom")])
+
+
+    path_publisher = Node(
+        package='path_publisher',
+        executable='publisher',
+        parameters=[
+            (path.join(param_dir,"planning","path_publisher.param.yaml"))]
+        remappings=[
+            ("paths", "paths"),
+            ("path_pub_viz", "path_pub_viz")])
+
+    unified_controller = Node(
+        package='unified_controller',
+        executable='unified_controller_node',
+        remappings=[
+            ("/planning/outgoing_trajectory", "trajectory"),
+            ("/sensors/gnss/odom", "odometry"),
+            ("/command/throttle_position", "throttle_position"),
+            ("/command/brake_position", "brake_position"),
+            ("/command/steering_position", "steering_target")])
+
+    motion_planner = Node(
+        package='motion_planner',
+        executable='motion_planner',
+        remappings=[
+            ("outgoing_trajectory", "trajectory"),
+            ("planning/paths", "paths"),
+            ("/planning/zones", "zones"),
+            ("/sensors/gnss/odom", "odometry")],
+        parameters=[
+            (path.join(param_dir,"planning","motion_planner.param.yaml"))])
+
+    zone_fusion = Node(
+        package='zone_fusion',
+        executable='ZoneFusionLaunch',
+        remappings=[
+            ("/planning/zone_array", "traffic_zones"),
+            ("/planning/obstacle_zone_array", "obstacle_zones"),
+            ("zones", "zones")])
+
+    obstacle_zoner = Node(
+        package='obstacle_zoner',
+        executable='ObstacleZonerLaunch',
+        remappings=[
+            ("obstacle_zone_array", "obstacle_zones"),
+            ("/sensors/zed/obstacle_array_3d", "zed_obstacles")])
+
+    behavior_planner = Node(
+        package='nova_behavior_planner',
+        executable='BehaviorPlannerLaunch',
+        parameters=[
+            (path.join(param_dir,"planning","path_publisher.param.yaml"))],
+        remappings=[
+            ("/sensors/gnss/odom", "odometry"),
+            ("paths", "paths"),
+            ("/sensors/zed/obstacle_array_3d", "zed_obstacles")])
+
+    # MISSING PIECES:
+    # zed
+    # lidar
+    # state estimation
+    # obstacle detection
+    # base link transform?
+    # visualization
     
     return LaunchDescription([
         serial,
         servo_throttle,
         servo_brake,
-        can,
+        epas_can,
         epas_reporter,
         epas_controller,
         steering_pid,
+        gnss_parser,
+        vehicle_can,
+        speedometer_reporter,
+        speedometer_translator,
+        path_publisher,
+        unified_controller,
+        motion_planner,
+        zone_fusion,
+        obstacle_zoner,
+        behavior_planner,
     ])
