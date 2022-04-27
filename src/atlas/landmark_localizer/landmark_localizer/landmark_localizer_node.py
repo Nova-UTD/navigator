e42e1dee2c0d8d8cc345e07e3ed87d56c19d9aaa
jim-works <jim4m80@gmail.com>
2022-04-27 18:19:04 -0500

    test

--- src/atlas/landmark_localizer/landmark_localizer/landmark_localizer_node.py
+++ src/atlas/landmark_localizer/landmark_localizer/landmark_localizer_node.py
@@ -29,14 +29,42 @@ class LandmarkLocalizerNode(Node):
     def __init__(self):
         super().__init__('landmark_localizer')
         self.get_logger().info("Hello, world!")
+        self.gnss = 1 #gps estimate
         self.load_landmarks()
         self.initPubSub()
         self.max_landmark_difference = 5.0 #only correct if a known landmark is less than x meters from the observed
-        self.corners_to_use = [0,3,4,7] #bounding box corners (should be facing the car to be more reliable) (not if this is correct)
-        
+        self.corners_to_use = [0,4] #we need to find two corners that correspond between the boxes. they should be distinct in the x/y plane
+        #cube with center = 1.5
+        obs = Obstacle3D()
+        obs.id = 0
+        obs.bounding_box.center.position.x = -0.5
+        obs.bounding_box.center.position.y = 1.5
+        obs.bounding_box.center.position.z = 0.5
+        obs.bounding_box.corners[4].x = 0.5
+        obs.bounding_box.corners[4].y = 1.5
+        obs.bounding_box.corners[7].x = 0.5
+        obs.bounding_box.corners[7].y = 1.5
+        obs.bounding_box.corners[0].x = 0.5
+        obs.bounding_box.corners[0].y = 2.5
+        obs.bounding_box.corners[3].x = 0.5
+        obs.bounding_box.corners[3].y = 2.5
+        self.landmark_cb(obs)
 
     def load_landmarks(self):
         self.landmarks = {}
+        obs = Obstacle3D()
+        obs.bounding_box.center.position.x = 1.5
+        obs.bounding_box.center.position.y = 0.5
+        obs.bounding_box.center.position.z = 0.5
+        obs.bounding_box.corners[4].x = 1.0
+        obs.bounding_box.corners[4].y = 0.0
+        obs.bounding_box.corners[7].x = 1.0
+        obs.bounding_box.corners[7].y = 0.0
+        obs.bounding_box.corners[0].x = 2.0
+        obs.bounding_box.corners[0].y = 0.0
+        obs.bounding_box.corners[3].x = 2.0
+        obs.bounding_box.corners[3].y = 0.0
+        self.landmarks[0] = [obs]
         #thinking a dict of List[Obstacle3D]. we can assign an id for the sign (the key of the dict) and record the bounding box
         pass
 
@@ -55,8 +83,8 @@ class LandmarkLocalizerNode(Node):
             self.get_logger().warn(f"Landmark recieved but no gnss!")
             return
         #TODO: tf landmark_msg from base_link using GNSS
-        landmark_msg_tf = tf_gnss(landmar_msg)
-        map_landmark, distance_from_observed = self.closest_landmark(landmark_msg)
+        landmark_msg_tf = landmark_msg#tf_gnss(landmar_msg)
+        map_landmark, distance_from_observed = self.closest_landmark(landmark_msg_tf)
         if map_landmark == None:
             self.get_logger().warn(f"No map landmark to compare!")
             return
@@ -66,10 +94,13 @@ class LandmarkLocalizerNode(Node):
         #compute correction using base_link landmark
         rot_mat = self.compute_rot(landmark_msg, map_landmark)
         trans = self.compute_trans(landmark_msg, map_landmark, rot_mat)
+        self.get_logger().info(f"{rot_mat}")
+        self.get_logger().info(f"{trans}")
         #publish tf
         self.calc_pub_tf(rot_mat, trans)
 
-        
+    def tf_gnss(self, landmark):
+        pass
     #computes the inverse rotation matrix by comparing the difference of pairs of points from each landmark
     #bl landmark is in base_link, map_landmark is in map
     #b = point on bl_landmark, m=point on map_landmark, R rotatation matrix
@@ -84,26 +115,30 @@ class LandmarkLocalizerNode(Node):
             b_j = bl_landmark.bounding_box.corners[j]
             m_i = map_landmark.bounding_box.corners[i]
             m_j = map_landmark.bounding_box.corners[j]
-            db = np.array([b_i[0]-b_j[0],b_i[1]-b_j[1],b_i[2]-b_j[2]])
-            dm = np.array([m_i[0]-m_j[0],m_i[1]-m_j[1],m_i[2]-m_j[2]])
+            db = np.array([b_i.x-b_j.x,b_i.y-b_j.y,0])
+            dm = np.array([m_i.x-m_j.x,m_i.y-m_j.y,0])
             #compute rotation matrix
-            sum_mat += rotatation_matrix_from_vectors(db, dm)
+            self.get_logger().info(f"db {db}")
+            self.get_logger().info(f"dm {dm}")
+            m = self.rotation_matrix_from_vectors(db, dm)
+            sum_mat += m 
+            self.get_logger().info(f"mat {m}")
             num_mat += 1
         #take the avereage
         return sum_mat/num_mat
     
     #should just construct a tf from the supplied parameters
     def calc_pub_tf(self, rot_mat, trans):
-        rot_q = R.from_matrix(rot_mat).as_quat() #x,y,z,w
-        br = tf2_ros.TransformBroadcaster()
-        t = geometry_msgs.msg.TransformStamped()
+        rot_q = R.from_dcm(rot_mat).as_quat() #x,y,z,w
+        br = tf2_ros.TransformBroadcaster(self)
+        t = TransformStamped()
    
-        t.header.stamp = rospy.Time.now()
+        t.header.stamp = self.get_clock().now().to_msg()
         t.header.frame_id = "base_link"
         t.child_frame_id = "landmark_tf"
-        t.transform.translation.x = trans.x
-        t.transform.translation.y = trans.y
-        t.transform.translation.z = trans.z
+        t.transform.translation.x = trans[0]
+        t.transform.translation.y = trans[1]
+        t.transform.translation.z = trans[2]
         t.transform.rotation.x = rot_q[0]
         t.transform.rotation.y = rot_q[1]
         t.transform.rotation.z = rot_q[2]
@@ -119,18 +154,20 @@ class LandmarkLocalizerNode(Node):
         for i in self.corners_to_use:
             b_i = bl_landmark.bounding_box.corners[i]
             m_i = map_landmark.bounding_box.corners[i]
-            bl = np.array([b_i[0], b_i[1], b_i[2]])
-            mp = np.array([m_i[0], m_i[1], m_i[2]])
+            bl = np.array([b_i.x, b_i.y, b_i.z])
+            mp = np.array([m_i.x, m_i.y, m_i.z])
             sum_t += inverse_rot.dot(mp) - bl
         return sum_t/len(self.corners_to_use)
 
     
-    def rotation_matrix_from_vectors(vec1, vec2):
+    def rotation_matrix_from_vectors(self, vec1, vec2):
         """ Find the rotation matrix that aligns vec1 to vec2
         :param vec1: A 3d "source" vector
         :param vec2: A 3d "destination" vector
         :return mat: A transform matrix (3x3) which when applied to vec1, aligns it with vec2.
         """
+        if np.linalg.norm(vec1-vec2) < 0.01:
+            return np.eye(3) #do not do the calculation if the vectosr are very close
         a, b = (vec1 / np.linalg.norm(vec1)).reshape(3), (vec2 / np.linalg.norm(vec2)).reshape(3)
         v = np.cross(a, b)
         c = np.dot(a, b)
