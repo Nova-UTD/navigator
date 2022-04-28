0f0f462bc40261e0b8662e254bfa720584d43787
Egan Johnson <eg.co.johnson@gmail.com>
2022-04-28 01:33:36 -0500

    node skeleton

---
+++ src/atlas/curb_localizer/include/curb_localizer/CurbLocalizerNode.hpp
@@ -0,0 +1,29 @@
+/*
+ * Package:   curb_localizer
+ * Filename:  CurbLocalizerNode.hpp
+ * Author:    Egan Johnson
+ * Email:     egan.johnson@utdallas.edu
+ * Copyright: 2022, Voltron UTD
+ * License:   MIT License
+ */
+
+# pragma once
+
+# include <rclcpp/rclcpp.hpp>
+
+
+namespace navigator
+{
+namespace curb_localizer
+{
+
+class CurbLocalizerNode : public rclcpp::Node
+{
+    public:
+        CurbLocalizerNode();
+
+    private:
+
+};
+}
+} // namespace navigator
---
+++ src/atlas/curb_localizer/src/CurbLocalizerNode.cpp
@@ -0,0 +1,16 @@
+/*
+ * Package:   curb_localizer
+ * Filename:  CurbLocalizerNode.cpp
+ * Author:    Egan Johnson
+ * Email:     egan.johnson@utdallas.edu
+ * Copyright: 2022, Voltron UTD
+ * License:   MIT License
+ */
+
+#include "curb_localizer/CurbLocalizerNode.hpp"
+
+using namespace navigator::curb_localizer;
+
+CurbLocalizerNode::CurbLocalizerNode() : Node("curb_localizer")
+{
+}
