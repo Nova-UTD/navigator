dfd3c34f2e896c524d4d7c8e3ae9bd3470380c5e
Will Heitman <Will.Heitman@utdallas.edu>
2022-09-07 11:52:04 -0500

    Delete additional stale packages

--- src/interface/sim_bridge/sim_bridge/sim_bridge_node.py
+++ src/interface/sim_bridge/sim_bridge/sim_bridge_node.py
@@ -58,6 +58,7 @@ import sim_bridge.scenarios as sc
 # GLOBAL CONSTANTS
 # TODO: Move to ROS param file, read on init. WSH.
 CLIENT_PORT = 2000
+CLIENT_WORLD = 'Town07'
 
 GNSS_PERIOD = 1/(2.0)  # 2 Hz
 GROUND_TRUTH_OBJ_PERIOD = 1/(2.0)  # 2 Hz (purposely bad)
@@ -622,13 +623,9 @@ class SimBridgeNode(Node):
     def connect_to_carla(self):
         # Connect to client, load world
         self.get_logger().info("Connecting to CARLA on port {}".format(CLIENT_PORT))
-        self.client = carla.Client('localhost', CLIENT_PORT)
-        self.client.set_timeout(20.0)
-        
-        scenario_manager = sc.ScenarioManager(self)
-        scenario_manager.car_stopped_at_stop_junction()
-
-        self.get_logger().info("Started scenario!")
+        client = carla.Client('localhost', CLIENT_PORT)
+        client.set_timeout(20.0)
+        self.world = client.load_world(CLIENT_WORLD)
         
 
     def add_ego_sensors(self):
