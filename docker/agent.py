5432e83eae890379b0f6646a1f8b5272b896e9da
Will Heitman <will.heitman@utdallas.edu>
2022-10-31 10:26:29 -0500

    Update agent files for leaderboard

--- docker/agent.py
+++ docker/agent.py
@@ -4,17 +4,18 @@ from leaderboard.autoagents.autonomous_agent import Track
 def get_entry_point():
     return 'NavigatorAgent'
 
+
 class NavigatorAgent(ROS2Agent):
     
     def get_ros_entrypoint(self):
         return {
-            "package": "carla",
-            "launch_file": "carla.launch.py",
+            "package": "my_ros_agent",
+            "launch_file": "my_ros_agent.launch",
             "parameters": {}
         }
 
     def setup(self, path_to_conf_file):
-        self.track = Track.MAP # At a minimum, this method sets the Leaderboard modality. In this case, SENSORS
+        self.track = Track.SENSORS # At a minimum, this method sets the Leaderboard modality. In this case, SENSORS
 
     def sensors(self):
         sensors = [
@@ -22,8 +23,8 @@ class NavigatorAgent(ROS2Agent):
             'x': 0.7, 'y': 0.0, 'z': 1.60, 'roll': 0.0, 'pitch': 0.0, 'yaw': 0.0, 'width': 300, 'height': 200, 'fov': 100},
             {'type': 'sensor.lidar.ray_cast', 'id': 'LIDAR',
             'x': 0.7, 'y': -0.4, 'z': 1.60, 'roll': 0.0, 'pitch': 0.0, 'yaw': -45.0},
-            # {'type': 'sensor.other.radar', 'id': 'RADAR',
-            # 'x': 0.7, 'y': -0.4, 'z': 1.60, 'roll': 0.0, 'pitch': 0.0, 'yaw': -45.0, 'fov': 30},
+            {'type': 'sensor.other.radar', 'id': 'RADAR',
+            'x': 0.7, 'y': -0.4, 'z': 1.60, 'roll': 0.0, 'pitch': 0.0, 'yaw': -45.0, 'fov': 30},
             {'type': 'sensor.other.gnss', 'id': 'GPS',
             'x': 0.7, 'y': -0.4, 'z': 1.60},
             {'type': 'sensor.other.imu', 'id': 'IMU',
