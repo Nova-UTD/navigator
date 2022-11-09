e4f469cc54e2b861c82d1d6a48f3ade30e932010
Will Heitman <will.heitman@utdallas.edu>
2022-11-09 12:24:18 -0600

    Update voltron_msgs and main.yml

--- docker/build_container.sh
+++ docker/build_container.sh
@@ -1,1 +1,8 @@
-colcon build --packages-select cyclonedds --cmake-clean-cache && . install/setup.sh && colcon build --cmake-clean-cache
+docker run \
+    -it \
+    --rm \
+    -v $PWD:/navigator \
+    --net=host \
+    -e="DISPLAY" \
+    novautd/navigator:latest \
+    "apt update && apt install -y ros-foxy-rmw-cyclonedds-cpp && colcon build --cmake-clean-cache"
