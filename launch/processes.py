11d3bb22f96db4af650e1f9ca28ac75dc8f2f492
Raghav Pillai <rpillai@justingpu.utdallas.edu>
2022-11-09 18:03:57 -0600

    New process handler

--- launch/processes.py
+++ launch/processes.py
@@ -1,8 +1,109 @@
+import logging
+import subprocess
+import time
 from launch.text_colors import text_colors as colors
+
+TIME_FROM_LAST_CHECK = time.time() # Time from last sanity check
+SANITY_FREQUENCY = 1 # Sanity check frequency in seconds
 
 processes = {} # Holder for processes
 
+def initialize_process(package_name: str, process_name: str) -> None:
+    """
+    Initialized process
+    @param package_name[str]    Package name of node
+    @param process_name[str]    Name of process/executable of node
+    """
+    processes[package_name] = "SUCCESS"
+    print(f"{colors.HEADER}Successfully initialized Node {colors.CYAN}<{package_name}>{colors.ENDC} EXECUTED AS ({process_name})")
+
+def change_process_status(package_name: str, status: str) -> None:
+    """
+    Sets a process to a certain status
+    @param package_name[str]    Package name of node
+    @param status[str]          Status of node to set
+    """
+    if package_name == "launch": 
+        return
+    if processes[package_name] != status:
+        processes[package_name] = status
+        #print(f"{colors.CYAN}<{package_name}>{colors.ENDC} node changed to state {colors.HEADER}{status}{colors.ENDC}")
+
+def check_processes() -> None:
+    """
+    Loops through processes and checks all
+    """
+    for package_name, status in processes.items():
+        if status != "SUCCESS":
+            color = colors.WARNING
+            if status == "FATAL":
+                color = colors.FAIL
+            print(f"{colors.CYAN}<{package_name}>{colors.ENDC} status is currently {color}{status}{colors.ENDC}")
+
+def call_node_sanity_check() -> None:
+    """
+    Calls sanity check, checks if time from previous check was greater than SANITY_FREQUENCY
+    """
+    global TIME_FROM_LAST_CHECK
+    if (time.time() - TIME_FROM_LAST_CHECK) > SANITY_FREQUENCY: # See if elapsed time from last sanity check was over 1 second
+        TIME_FROM_LAST_CHECK = time.time()
+        perform_node_sanity_check()
+
+def perform_node_sanity_check() -> None:
+    """
+    Checks what nodes are active and compares to initial node list
+    """
+
+    found_nodes: dict = {} # Dictionary to keep track of current nodes
+    
+    for exec_name, node_name in nodes.items(): # Traverse through node dictionary
+        if node_name != "launch": # Get all processes except for launch executable
+            found_nodes[node_name] = False
+
+    process: subprocess = subprocess.Popen(
+        "ros2 node list",
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        executable="/bin/bash",
+        shell=True
+    )
+
+    for line in iter(process.stdout.readline, ''): # Loop through piped subprocess output
+        line: str = line.decode('utf-8').rstrip() # Decode binary string to utf-8 and remove whitespace
+        if line == "" and line is not None: # Break from loop if null or empty string encountered (if process completed)
+            break
+        
+        node_name: str = line.rsplit('/', 1)[-1] # Split raw node name from namespace and get package name
+        
+        for node in found_nodes: # Loop through initially launched nodes
+            if node in node_name: # If we find our node alive, then set alive to True
+                found_nodes[node] = True
+
+    for name, value in found_nodes.items(): # Loop through initially launched nodes
+        if value == True: # If node value is true, then it's still alive
+            pass
+            #print(f"<{name}> node is in state SUCCESS")
+        else:
+            if name != "launch" and name != "ros_client_library":
+                change_process_status(name, "FATAL")
+
+    check_processes()
+
+
+def get_node_from_process(process: str) -> str:
+    """
+    Takes an executable process name from console and matches with node list [ros2 node list] 
+    @param process[str]     String process to parse
+    """
+    try: # Try catch to return process if exists, or show error if not
+        return nodes[process]
+    except:
+        print(f"{colors.FAIL}{colors.BOLD}<{process}> NOT FOUND, PACKAGE NOT DEFINED{colors.ENDC}")
+        return process
+
 nodes: dict = { # Node dictionary for converting node executable to package name
+    "rcl": "ros_client_library",
     "launch": "launch",
     "unified_controller_node": "unified_controller_node",
     "ukf_node": "localization_map_odom",
@@ -16,14 +117,3 @@ nodes: dict = { # Node dictionary for converting node executable to package name
     "publisher": "path_publisher_node",
     "motion_planner": "motion_planner_node"
 }
-
-def get_node_from_process(process: str) -> str:
-    """
-    Takes an executable process name from console and matches with node list [ros2 node list] 
-    @param process[str]     String process to parse
-    """
-    try: # Try catch to return process if exists, or show error if not
-        return nodes[process]
-    except:
-        print(f"{colors.FAIL}{colors.BOLD}<{process}> NOT FOUND, PACKAGE NOT DEFINED{colors.ENDC}")
-        return process
