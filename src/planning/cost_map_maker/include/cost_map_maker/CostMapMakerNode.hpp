c28beb481f8da2cf40e9302014c7d5e058926acf
Chtun <chtun@live.com>
2022-11-11 13:22:52 -0600

    Functioning CostMapMaker Node inputs and outputs

--- src/planning/cost_map_maker/include/cost_map_maker/CostMapMakerNode.hpp
+++ src/planning/cost_map_maker/include/cost_map_maker/CostMapMakerNode.hpp
@@ -8,23 +8,34 @@
  */
 
 /*
-    Currently, this node republishes data from the path publisher. In the future,
-    It will take in zones from the traffic planner and obstacle zoner
-    and assign speeds to the path.
+    Currently, this node gets an Evidential Grid Map input from perception and creates a cost map accordingly to ensure safety and good driving practices.
+    It does so by giving collisions an extremely high costs and making spaces closer to the next waypoint less costly. *TO BE CONTINUED*
 */
 
 #pragma once
 
 #include <chrono> // Time literals
 #include <vector>
+#include <string>
 
+// libOpenDRIVE includes
+#include "OpenDriveMap.h"
+#include "Lanes.h"
+#include "Road.h"
+#include "Geometries/Line.h"
+#include "RefLine.h"
+#include "opendrive_utils/OpenDriveUtils.hpp"
+
+// Message includes
 #include "rclcpp/rclcpp.hpp"
 #include <nav_msgs/msg/odometry.hpp>
-#include "voltron_msgs/msg/zone.hpp"
-#include "voltron_msgs/msg/zone_array.hpp"
+#include <visualization_msgs/msg/marker.hpp>
+#include <visualization_msgs/msg/marker_array.hpp>
+#include <geometry_msgs/msg/vector3.hpp>
 #include "voltron_msgs/msg/egma.hpp"
 #include "voltron_msgs/msg/evidential_grid.hpp"
-#include "voltron_msgs/msg/gps_diagnostic.hpp"
+#include <visualization_msgs/msg/marker.hpp>
+#include <visualization_msgs/msg/marker_array.hpp>
 
 using namespace std::chrono_literals;
 
@@ -48,7 +59,7 @@ private:
     // Subscription to *INSERT PUBLISHER* for input Evidential Grid Map
     void update_egma(voltron_msgs::msg::Egma::SharedPtr ptr);
     // Subscription to *INSERT PUBLISHER* for input waypoints
-    void update_waypoints(voltron_msgs::msg::ZoneArray::SharedPtr ptr);
+    void update_waypoints(geometry_msgs::msg::Vector3::SharedPtr ptr);
     // Subscription to *INSERT PUBLISHER* for the current heading of the car
     void odometry_pose_cb(const nav_msgs::msg::Odometry::SharedPtr msg);
 
@@ -57,7 +68,7 @@ private:
     // Evidential Grid Map Subscriber
     rclcpp::Subscription<voltron_msgs::msg::Egma>::SharedPtr egma_subscription;
     /* TODO Add information from global waypoints */
-    rclcpp::Subscription<voltron_msgs::msg::ZoneArray>::SharedPtr waypoint_subscription;
+    rclcpp::Subscription<geometry_msgs::msg::Vector3>::SharedPtr waypoint_subscription;
 
     // Odometry Subscriber
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odomtery_pose_subscription;
@@ -65,10 +76,13 @@ private:
     rclcpp::TimerBase::SharedPtr control_timer;
 
 
+    // EGMa from prediction
     voltron_msgs::msg::Egma::SharedPtr egma;
+
     /* TODO Add information from global waypoints */
-    voltron_msgs::msg::ZoneArray::SharedPtr waypoints;
-    voltron_msgs::msg::Odometry::SharedPtr odometry;
+    navigator::opendrive::OpenDriveMapPtr carla_map;
+    geometry_msgs::msg::Vector3::SharedPtr waypoints;
+    nav_msgs::msg::Odometry::SharedPtr odometry;
 };
 }
 }
--- src/planning/cost_map_maker/src/CostMapMakerNode.cpp
+++ src/planning/cost_map_maker/src/CostMapMakerNode.cpp
@@ -15,35 +15,32 @@
 #include <boost/geometry.hpp>
 
 #include "cost_map_maker/CostMapMakerNode.hpp"
-#include "voltron_msgs/msg/zone.hpp"
-#include "zone_lib/zone.hpp"
 
 #include <list>
 #include <algorithm>
 
 using namespace navigator::cost_map_maker;
-using namespace navigator::zones_lib;
 
-using ZoneArray = voltron_msgs::msg::ZoneArray;
-using Zone = voltron_msgs::msg::Zone;
 using voltron_msgs::msg::Egma;
 using voltron_msgs::msg::EvidentialGrid;
-
+using geometry_msgs::msg::Vector3;
 
 CostMapMakerNode::CostMapMakerNode() : Node("cost_map_maker_node")
 {
-    cost_map_publisher = this->create_publisher<voltron_msgs::msg::Egma>("outgoing_cost_map", 8);
-    egma_subscription = this->create_subscription<voltron_msgs::msg::Egma>("*INSERT SUBSCRIPTION*", 10, bind(&MotionPlannerNode::update_egma, this, std::placeholders::_1));
-    waypoint_subscription = this->create_subscription<ZoneArray>("*INSERT SUBSCRIPTION*", 10, bind(&MotionPlannerNode::update_waypoints, this, std::placeholders::_1));
-    odomtery_pose_subscription = this->create_subscription<nav_msgs::msg::Odometry>("/carla/odom", rclcpp::QoS(10),std::bind(&MotionPlannerNode::odometry_pose_cb, this, std::placeholders::_1));
-    //current_pose_subscription = this->create_subscription<VehicleKinematicState>("vehicle_kinematic_state", rclcpp::QoS(10), std::bind(&MotionPlannerNode::current_pose_cb, this, std::placeholders::_1));
-    //steering_angle_subscription = this->create_subscription<voltron_msgs::msg::SteeringPosition>("/can/steering_angle", 8, bind(&MotionPlannerNode::update_steering_angle, this, std::placeholders::_1));
-    control_timer = this->create_wall_timer(message_frequency, bind(&MotionPlannerNode::send_message, this));
+    std::string xodr_path = "data/maps/town07/Town07_Opt.xodr";
     
-    //planner = std::make_shared<MotionPlanner>();
+    cost_map_publisher = this->create_publisher<voltron_msgs::msg::Egma>("outgoing_cost_map", 8);
+    egma_subscription = this->create_subscription<voltron_msgs::msg::Egma>("*INSERT SUBSCRIPTION*", 10, bind(&CostMapMakerNode::update_egma, this, std::placeholders::_1));
+    waypoint_subscription = this->create_subscription<geometry_msgs::msg::Vector3>("*INSERT SUBSCRIPTION*", 10, bind(&CostMapMakerNode::update_waypoints, this, std::placeholders::_1));
+    odomtery_pose_subscription = this->create_subscription<nav_msgs::msg::Odometry>("/carla/odom", rclcpp::QoS(10),std::bind(&CostMapMakerNode::odometry_pose_cb, this, std::placeholders::_1));
+    control_timer = this->create_wall_timer(message_frequency, bind(&CostMapMakerNode::send_message, this));
+    
+    // Read map from file, using our path param
+	RCLCPP_INFO(this->get_logger(), "Reading from ", xodr_path.c_str());
+	carla_map = navigator::opendrive::load_map(xodr_path)->map;
 }
 
-void MotionPlannerNode::send_message() {
+void CostMapMakerNode::send_message() {
     if (egma == nullptr) {
         return;
     }
@@ -52,7 +49,7 @@ void MotionPlannerNode::send_message() {
     for (size_t time_index = 0; time_index < egma->egma.size(); time_index++) {
       auto cost_map_single_timestep = voltron_msgs::msg::EvidentialGrid();
       auto current_evidential_grid = egma->egma[time_index];
-      cost_map_single_timestep.timestep = current_evidential_grid.timestep;
+      cost_map_single_timestep.header = current_evidential_grid.header;
       for (size_t coordinate_index = 0; coordinate_index < current_evidential_grid.grid.size(); coordinate_index++)
       {
         cost_map_single_timestep.grid[coordinate_index].occupancy_value = current_evidential_grid.grid[coordinate_index].occupancy_value;
@@ -60,21 +57,21 @@ void MotionPlannerNode::send_message() {
       }
       cost_map.egma.push_back(cost_map_single_timestep);
     }
-    trajectory_publisher->publish(cost_map_egma);
+    cost_map_publisher->publish(cost_map);
 
 
     return;
 }
 
-void MotionPlannerNode::update_egma(voltron_msgs::msg::Egma::SharedPtr ptr) {
+void CostMapMakerNode::update_egma(voltron_msgs::msg::Egma::SharedPtr ptr) {
     egma = ptr;
 }
 
-void MotionPlannerNode::update_waypoints(voltron_msgs::msg::ZoneArray::SharedPtr ptr)
+void CostMapMakerNode::update_waypoints(geometry_msgs::msg::Vector3::SharedPtr ptr)
 {
-
+    waypoints = ptr;
 }
 
-void MotionPlannerNode::odometry_pose_cb(const nav_msgs::msg::Odometry::SharedPtr msg) {
+void CostMapMakerNode::odometry_pose_cb(const nav_msgs::msg::Odometry::SharedPtr msg) {
     odometry = msg;
 }
