7c446e8e33ff394e415aad3b6808cc6f3f3caec0
santonyuk <stepan.antonyuk@gmail.com>
2022-11-15 15:06:35 -0600

    fix: colcon build --symlink-install now works with the node

--- src/perception/ground_seg/setup.py
+++ src/perception/ground_seg/setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup
 
+package_name = 'ground_seg'
+
 setup(
-    name='ground_seg',
+    name=package_name, 
     version='1.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resources_index/packages', ['resource/' + package_name]),
+        ('share/' + package_naem, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
     zip_safe=True,
     maintainer='santonyuk',
     maintainer_email='stepan.antonyuk@gmil.com',
     description='Deletes all the ground points',
     license='MIT',
-    install_requires=['setuptools','numpy'],
-     )
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': ['ground_seg = ground_seg.ground_seg:main'],
+    },
+)
