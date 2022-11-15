f0bf03e043afd332f00b035911abe47c996cca81
santonyuk <stepan.antonyuk@gmail.com>
2022-11-15 15:38:07 -0600

    fix: ros2 run ground_seg ground_seg works now

--- src/perception/ground_seg/setup.py
+++ src/perception/ground_seg/setup.py
@@ -7,8 +7,8 @@ setup(
     version='1.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resources_index/packages', ['resource/' + package_name]),
-        ('share/' + package_naem, ['package.xml']),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
