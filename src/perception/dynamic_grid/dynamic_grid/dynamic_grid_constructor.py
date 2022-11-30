e4630461d25f7f464f6047469312d81b535e237d
ashwinsom <ashwin.som2001@gmail.com>
2022-11-30 13:41:47 -0600

    Implemented shadow casting for the static grid

--- src/perception/dynamic_grid/dynamic_grid/dynamic_grid_constructor.py
+++ src/perception/dynamic_grid/dynamic_grid/dynamic_grid_constructor.py
@@ -7,6 +7,7 @@ import sys
 import os
 import math
 import grid_cell
+from shadowcasting import ShadowCaster
 
 import rclpy
 from rclpy.node import Node
@@ -27,35 +28,56 @@ class DynamicGridConstructor(Node):
 		pcd.points = o3d.utility.Vector3dVector(data)
 		arr = np.asarray(pcd.points)
 		print(arr.size)
-		occ = pcd_to_occupancy_grid(arr)
-		print(type(occ))
-		print(occ.size)
-		numpy_to_image(occ, os.path.join(RESULT_PATH, 'image'))
+		occ = pcd_to_sensor_grid(arr)
+
+		width = 120
+		height = 120
+		fov = ShadowCaster(width, height)
+		for y in range(height):
+			for x in range(width):
+				if x == 0 or y == 0 or x == width - 1 or y == height - 1:
+					occ[x][y] = False
+
+		
+		result = fov.cast_shadow(occ, 59, 59, 240)
+		static_occ = np.zeros([height, width])
+		for y in range(height):
+			for x in range(width):
+				if result[x][y]:
+					if occ[x][y]:
+						static_occ[x][y] = 0
+					else:
+						static_occ[x][y] = 1
+				else:
+					static_occ[x][y] = 1
+
+		
+		return static_occ
+	
+	def pcd_flattener(points: np.ndarray):
+		for point in points:
+			points[2] = 0
+		return points
+	
+	def pcd_to_sensor_grid(points: np.ndarray):
+		rows = 80
+		cols = 120
+		#this grid creates the base for the egma we will send over to BPC
+		sensor_grid = [[True for i in range(cols)] for j in range(rows)]
+		for point in points:
+			x_cor = point[0]
+			y_cor = point[1]
+			x_cor = math.ceil(x_cor)
+			y_cor = math.ceil(y_cor)
+			sensor_grid[x_cor][y_cor] = False
+		
+		return sensor_grid
+		#return np.array(sensor_grid)
+
 	
 	def grid_to_image(arr: np.ndarray, path: str):
     	cv2.imwrite(f'{path}.png',(arr * 255).astype(np.uint8))
 
-	def pcd_to_occupancy_grid(pcd: np.ndarray):
-    
-		pcd = pcd.round()
-
-		x_min = pcd[:, 0].min()
-		x_max = pcd[:, 0].max()
-
-		y_min = pcd[:, 1].min()
-		y_max = pcd[:, 1].max()
-
-
-		occ = np.zeros((int(x_max - x_min + 1), int(y_max - y_min + 1)), dtype=np.float64)
-
-		for i in range(pcd.shape[0]):
-			x = int(pcd[i, 0] - x_min)
-			y = int(pcd[i, 1] - y_min)
-
-			occ[x, y] += 1
-
-		occ /= pcd.shape[0]
-		return occ
 
 
 def main(args=None):
