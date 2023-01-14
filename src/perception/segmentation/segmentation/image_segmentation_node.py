3201f23e6f252ecbea055da436bc2f21209d7b4a
Will Heitman <will.heitman@utdallas.edu>
2023-01-13 19:15:17 -0600

    Add mmseg to image_segmentation_node (again)

--- src/perception/segmentation/segmentation/image_segmentation_node.py
+++ src/perception/segmentation/segmentation/image_segmentation_node.py
@@ -8,10 +8,10 @@ Node to semantically segment a 2D image using a model.
 
 
 import rclpy
-import ros2_numpy as rnp
+# import ros2_numpy as rnp
 import numpy as np
 from rclpy.node import Node
-from scipy.spatial.transform import Rotation as R
+# from scipy.spatial.transform import Rotation as R
 import time
 from tf2_ros import TransformException
 from tf2_ros.buffer import Buffer
@@ -20,28 +20,108 @@ from tf2_ros.transform_listener import TransformListener
 # Message definitions
 from nav_msgs.msg import OccupancyGrid
 from rosgraph_msgs.msg import Clock
-from sensor_msgs.msg import PointCloud2
+from sensor_msgs.msg import Image, PointCloud2
 from std_msgs.msg import Float32
 
 import matplotlib.pyplot as plt
 
 # OpenMMSegmentation
-import torch
-import torchvision
-import mmseg
-from mmseg.apis import inference_segmentor, init_segmentor, show_result_pyplot
-from mmseg.core.evaluation import get_palette
-print(mmseg.__version__)
+from mmseg.apis import inference_segmentor, init_segmentor
+import mmcv
 
-print(torch.__version__, torch.cuda.is_available())
-# from mmseg.apis import inference_segmentor, init_segmentor, show_result_pyplot
+name_to_dtypes = {
+    "rgb8":    (np.uint8,  3),
+    "rgba8":   (np.uint8,  4),
+    "rgb16":   (np.uint16, 3),
+    "rgba16":  (np.uint16, 4),
+    "bgr8":    (np.uint8,  3),
+    "bgra8":   (np.uint8,  4),
+    "bgr16":   (np.uint16, 3),
+    "bgra16":  (np.uint16, 4),
+    "mono8":   (np.uint8,  1),
+    "mono16":  (np.uint16, 1),
 
+    # for bayer image (based on cv_bridge.cpp)
+    "bayer_rggb8":  (np.uint8,  1),
+    "bayer_bggr8":  (np.uint8,  1),
+    "bayer_gbrg8":  (np.uint8,  1),
+    "bayer_grbg8":  (np.uint8,  1),
+    "bayer_rggb16":     (np.uint16, 1),
+    "bayer_bggr16":     (np.uint16, 1),
+    "bayer_gbrg16":     (np.uint16, 1),
+    "bayer_grbg16":     (np.uint16, 1),
+
+    # OpenCV CvMat types
+    "8UC1":    (np.uint8,   1),
+    "8UC2":    (np.uint8,   2),
+    "8UC3":    (np.uint8,   3),
+    "8UC4":    (np.uint8,   4),
+    "8SC1":    (np.int8,    1),
+    "8SC2":    (np.int8,    2),
+    "8SC3":    (np.int8,    3),
+    "8SC4":    (np.int8,    4),
+    "16UC1":   (np.uint16,   1),
+    "16UC2":   (np.uint16,   2),
+    "16UC3":   (np.uint16,   3),
+    "16UC4":   (np.uint16,   4),
+    "16SC1":   (np.int16,  1),
+    "16SC2":   (np.int16,  2),
+    "16SC3":   (np.int16,  3),
+    "16SC4":   (np.int16,  4),
+    "32SC1":   (np.int32,   1),
+    "32SC2":   (np.int32,   2),
+    "32SC3":   (np.int32,   3),
+    "32SC4":   (np.int32,   4),
+    "32FC1":   (np.float32, 1),
+    "32FC2":   (np.float32, 2),
+    "32FC3":   (np.float32, 3),
+    "32FC4":   (np.float32, 4),
+    "64FC1":   (np.float64, 1),
+    "64FC2":   (np.float64, 2),
+    "64FC3":   (np.float64, 3),
+    "64FC4":   (np.float64, 4)
+}
 
 class ImageSegmentationNode(Node):
 
     def __init__(self):
         super().__init__('image_segmentation_node')
+        config_file = '/mmsegmentation/pspnet_r50-d8_512x1024_40k_cityscapes.py'
+        checkpoint_file = '/mmsegmentation/pspnet_r50-d8_512x1024_40k_cityscapes_20200605_003338-2966598c.pth'
 
+        self.model = init_segmentor(config_file, checkpoint_file, device='cuda:0')
+
+        self.front_rgb_sub = self.create_subscription(Image, "/carla/hero/rgb_center/image", self.rgb_front_cb, 10)
+
+        img = mmcv.imread('/navigator/demo.png')
+        
+        self.idx = 0
+        # model.show_result(img, result, out_file='result.jpg', opacity=1.0)
+
+    def image_to_numpy(self, msg):
+        if not msg.encoding in name_to_dtypes:
+            raise TypeError('Unrecognized encoding {}'.format(msg.encoding))
+
+        dtype_class, channels = name_to_dtypes[msg.encoding]
+        dtype = np.dtype(dtype_class)
+        dtype = dtype.newbyteorder('>' if msg.is_bigendian else '<')
+        shape = (msg.height, msg.width, channels)
+
+        data = np.frombuffer(msg.data, dtype=dtype).reshape(shape)
+        data.strides = (
+            msg.step,
+            dtype.itemsize * channels,
+            dtype.itemsize
+        )
+
+        if channels == 1:
+            data = data[...,0]
+        return data
+
+    def rgb_front_cb(self, msg: Image):
+        img_array = self.image_to_numpy(msg)
+        self.get_logger().info(str(img_array))
+        result = inference_segmentor(self.model, img_array)
 
 def main(args=None):
     rclpy.init(args=args)
