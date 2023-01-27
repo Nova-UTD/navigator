0552c87c086e1b14ccea23ceeb6365e5af8de3df
Jesse Musa <jessemusa2@gmail.com>
2023-01-27 16:28:13 -0600

    Testing stage for prednet node

--- src/perception/prednet_inference/prednet_inference/prednet_inference_node.py
+++ src/perception/prednet_inference/prednet_inference/prednet_inference_node.py
@@ -12,6 +12,7 @@ to right-sided ones. This node first merges
 these left- and right-sided PCDs into a complete
 cloud before cutting out points near the car.
 '''
+# https://1drv.ms/v/s!An_6l1bJUIotjlN-qg-uqKdIORgC?e=s5RDv6
 
 import rclpy
 import ros2_numpy as rnp
@@ -21,13 +22,14 @@ import time
 
 
 # Message definitions
-from nav_msgs.msg import OccupancyGrid
-
 from rosgraph_msgs.msg import Clock
 from std_msgs.msg import Float32
 
+from sensor_msgs.msg import PointCloud2
+from nav_msgs.msg import OccupancyGrid
 from nova_msgs.msg import Masses
 from nova_msgs.msg import Prediction
+
 
 import torch
 
@@ -38,9 +40,9 @@ import matplotlib.pyplot as plt
 class PredNetNode(Node):
 
     def __init__(self):
-        super().__init__('lidar_processing_node')
+        super().__init__('prednet_inference_node')
         self.masses_sub = self.create_subscription(
-            Masses, '/', self.semantic_lidar_cb, 10)
+            PointCloud2, '/lidar/filtered', self.masses_callback, 10)
 
         self.pred_all_pub = self.create_publisher(
             Prediction, '/', 10)
@@ -55,12 +57,37 @@ class PredNetNode(Node):
         
         self.sizeX = None
         self.sizeY = None
+        
+        
+        # Importing models
+        modelDir = "../models/prednet.pt"
+        
+        if torch.cuda.is_available():
+            print("CUDA available! Inference on GPU.")
+        else:
+            print("Inference on CPU.");  
+        
+        try:
+            self.prednet_model = torch.jit.load(modelDir)
+            self.prednet_model.cuda()
+            self.prednet_model.eval()
+        except:
+            raise Exception("Couldn't load prednet model")
+        
+        
+            
     
     def masses_callback(self, mass):
+        self.logger.info(self.time)
+        mass = Masses()
+        mass.width = 600
+        mass.height = 400
+        mass.occ = np.rand((mass.width * mass.height))
+        mass.free = np.rand((mass.width * mass.height))
         
         self.sizeX = mass.width
         self.sizeY = mass.height
-        
+    
         # Iterates Time
         self.time += 1
         
@@ -90,15 +117,33 @@ class PredNetNode(Node):
         return input_data
     
     def Infer(self, input):
-        return
+        output = self.prednet_model(input).to_Tensor()
+        
+        return output
     
     def Publish(self, output):
-        return
-    
-    
         
+        self.prediction_msg.clear()
         
-    
+        new_output = output.to("cpu")
+        
+        for i in range(new_output.shape[1]):
+            
+            occ_grid_msg = OccupancyGrid()
+            occ_grid_msg.info.resolution = 1. / 5.
+            occ_grid_msg.info.width = self.sizeX
+            occ_grid_msg.info.height = self.sizeY
+            
+            prob_2D = (new_output[0,i,0] * 0.5 + new_output[0,i,1] * 0.5) * 100
+            
+            occ_grid_msg.data = prob_2D.flatten()
+            
+            output_occ = new_output
+            
+            self.prediction_msg.prediction.append(occ_grid_msg)
+           
+        self.pred_all_pub.publish(self.prediction_msg)
+
 
 
 def main(args=None):
