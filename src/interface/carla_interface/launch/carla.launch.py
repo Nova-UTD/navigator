4fc91fbadade7a1ae6b1d9667038dbc3e75a459e
Will Heitman <will.heitman@utdallas.edu>
2023-02-09 12:22:07 -0600

    Fix memory leak, add docs to image_segmentation

--- src/interface/carla_interface/launch/carla.launch.py
+++ src/interface/carla_interface/launch/carla.launch.py
@@ -126,11 +126,11 @@ def generate_launch_description():
 
         # PERCEPTION
         image_segmentation,
-        semantic_projection,
-        lidar_processor,
-        ground_seg,
+        # semantic_projection,
+        # lidar_processor,
+        # ground_seg,
 
         # STATE ESTIMATION
-        map_manager,
-        gnss_processor,
+        # map_manager,
+        # gnss_processor,
     ])
