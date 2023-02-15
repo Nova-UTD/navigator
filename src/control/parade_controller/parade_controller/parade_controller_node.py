034151b6dd0df2712e8ef726db4dc9a963abb36e
danielv012 <daniel@vayman.co>
2023-02-14 19:38:26 -0600

    Filters pointcloud and finds banner x, y

--- src/control/parade_controller/parade_controller/parade_controller_node.py
+++ src/control/parade_controller/parade_controller/parade_controller_node.py
@@ -12,11 +12,15 @@ from rclpy.node import Node
 from sensor_msgs.msg import PointCloud2
 
 import rclpy
-import ros2_numpy as np
+import ros2_numpy as rnp
+import numpy as np
 
 class ParadeController(Node):
+
     def __init__(self):
         super().__init__('parade_controller_node')
+
+        self.INTENSITY_THRESHOLD = 0.75
 
         # Subscriber
         self.lidar_left_sub = self.create_subscription(PointCloud2, 'velodyne_points', self.pointclouds_cb, 10)
@@ -25,7 +29,27 @@ class ParadeController(Node):
         self.throttle_pub = self.create_publisher(CarlaEgoVehicleControl, '/carla/hero/vehicle_control_cmd', 10)
     
     def pointclouds_cb(self, msg: PointCloud2)
-        
+        pcd_temp: np.array = rnp.numpify(msg)
+        pcd: np.array
+
+        # Logic for combining 2 point clouds into pcd, maybe holding on to pointclouds until first point intensity is differet?
+        #
+        #
+        #
+
+        # Removes all points below the intensity threshold
+        pcd = pcd[pcd[:, 3] > self.INTENSITY_THRESHOLD]
+
+        # Finds the center of mass of banner points, resulting in a 1D numpy array (x, y) with the center coordinate of the banner
+        banner = np.mean(pcd[:, :2], axis = 0)
+
+        # TODO: Compute the distance between (0, 0) and the banner coordinate
+
+        # TODO: Controller logic
+
+        # TODO: Set msg fields and publish throttle value
+
+
         
 
 def main(args=None):
