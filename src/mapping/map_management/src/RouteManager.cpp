1b589a76eb6561ebac9ca93aa5c8357bb6cfdfde
Will Heitman <will.heitman@utdallas.edu>
2023-02-24 16:39:11 -0600

    Add to route generation refinements

---
+++ src/mapping/map_management/src/RouteManager.cpp
@@ -0,0 +1,18 @@
+#include "map_management/RouteManager.hpp"
+
+using namespace navigator::planning;
+
+RouteManager::RouteManager()
+{
+}
+
+/**
+ * @brief Returns a refined route given current position and the rough route
+ *
+ * @param rough A rough route, with a minimum of two waypoints
+ * @param pos Our current position
+ * @return A refined path, with smooth and rough sections, and with the route behind us removed.
+ */
+LineString RouteManager::getRoute(const LineString rough, const BoostPoint pos)
+{
+}
