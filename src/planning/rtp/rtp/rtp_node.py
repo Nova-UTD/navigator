fbf4a7f060b884fbf24b75398098db67a4bed39d
Will Heitman <will.heitman@utdallas.edu>
2023-02-28 16:34:25 -0600

    Finish recurvise tree planner

--- src/planning/rtp/rtp/rtp_node.py
+++ src/planning/rtp/rtp/rtp_node.py
@@ -36,8 +36,11 @@ import ros2_numpy as rnp
 from rosgraph_msgs.msg import Clock
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import ColorRGBA, String
-from visualization_msgs.msg import Marker
+from dataclasses import dataclass
+import random
+import time
+from geometry_msgs.msg import PoseStamped
+
 
 from matplotlib.patches import Rectangle
 
@@ -48,16 +51,24 @@ DEPTH: int = 3
 # These are vdehicle constants for the GEM e6.
 # The sim vehicle (Tesla Model 3) has similar constants.
 WHEEL_BASE: float = 3.5  # meters
-MAX_TURN_ANGLE = 0.47
+MAX_TURN_ANGLE = 0.3  # radians
+COST_CUTOFF = 90
 
 
+@dataclass
 class CostedPath:
-    points: np.ndarray
-    cost: int
+    poses = []  # pose = [x,y,heading]
+    cost = 0.0
 
-    def __init__(self) -> None:
-        self.points = np.asarray([])
-        self.cost = 0
+    def append(self, segment):
+        self.poses += segment.poses
+        self.cost += segment.cost
+
+    def copy(self):
+        new_path = CostedPath()
+        new_path.poses = self.poses.copy()
+        new_path.cost = self.cost
+        return new_path
 
 
 class RecursiveTreePlanner(Node):
@@ -70,7 +81,11 @@ class RecursiveTreePlanner(Node):
         odom_sub = self.create_subscription(
             Odometry, '/odometry/gnss_processed', self.odomCb, 1)
 
-        path_pub = self.create_publisher(
+        clock_sub = self.create_subscription(
+            Clock, '/clock', self.clockCb, 1)
+        self.clock = Clock()
+
+        self.path_pub = self.create_publisher(
             Path, '/planning/path', 1)
 
         self.ego_pose = None  # [x, y, heading]
@@ -80,44 +95,126 @@ class RecursiveTreePlanner(Node):
         #     Clock, '/clock', self.clockCb, 10)
         # self._cached_clock_ = Clock()
 
-    def getPaths(self, grid: np.ndarray, pose: np.ndarray, path_so_far: CostedPath, end_vec, depth: int) -> list:
-        if depth < 1:
+    def clockCb(self, msg: Clock):
+        self.clock = msg
+
+    def getSegment(self, inital_pose: np.ndarray, steering_angle, segment_length: float, res: float, costmap) -> CostedPath:
+        end_pose = np.copy(inital_pose)
+        segment_poses = []
+        current_length = 0.0
+        total_cost = 0
+        while current_length < segment_length:
+            end_heading = end_pose[2]
+
+            x, y = (res * np.cos(end_heading) + end_pose[0],
+                    res * np.sin(end_heading) + end_pose[1])
+
+            # Check cost at (x,y). Row-major means y is first!
+            if costmap.shape[0] < 1:
+                self.get_logger().warn("Costmap was empty")
+                return
+            cost = costmap[int(y), int(x)]
+            if cost > COST_CUTOFF:
+                return
+
+            total_cost += cost
+
+            segment_poses.append([
+                x, y, end_heading + steering_angle
+            ])
+            end_pose = segment_poses[-1]
+            current_length += res
+
+        path = CostedPath()
+        path.poses = segment_poses
+        path.cost = total_cost
+
+        return path
+
+    def generatePaths(self, depth: int, path: CostedPath, steering_angle, segment_length, res, num_branches, results: list, result_costs, costmap):
+        # Current pose at this step is the latest pose in the current path
+        pose = path.poses[-1]
+        segment = self.getSegment(
+            pose, steering_angle, segment_length, res, costmap)
+
+        if segment is None:  # getSegment hit an obstacle
             return
 
-        for steering_value in np.linspace(-1.0, 1.0, N_BRANCHES):
-            new_path = np.copy(path_so_far)
-            angle = steering_value * MAX_TURN_ANGLE
+        new_path: CostedPath = path.copy()
 
-            # Add a path based on the steering angle
-            radius = WHEEL_BASE/np.tan(angle)
+        new_path.append(segment)
 
-            t = 0.0
-            pts = []
+        if depth == 0:
+            results.append(new_path)
 
-            while t < STEP_LEN:
-                x = radius*np.cos(t)
-                y = radius*np.sin(t)
-                pts.append([x, y])
-                t += 1.0  # resolution in meters
+            if random.randint(0, 20) == 0:  # Only plot 5% of results
+                poses_np = np.asarray(new_path.poses)
+                # plt.plot(poses_np[:, 0], poses_np[:, 1])
+            return
 
-            plt.plot(pts)
-            plt.show()
+        for angle in np.linspace(-MAX_TURN_ANGLE, MAX_TURN_ANGLE, num_branches):
+            self.generatePaths(depth-1, new_path, angle, segment_length,
+                               res, num_branches, results, result_costs, costmap)
 
-            self.getPaths(grid, pose, new_path, depth=depth-1)
+    def startGeneration(self, costmap: np.ndarray, depth=7, segment_length=9.0, branches=7):
+        # plt.figure(figsize=(8, 6), dpi=160)
+        # plt.axes().set_aspect('equal')
+        # print(f"Generating path with depth {depth}, seg len {segment_length}")
+        origin = (50., 75.)
+
+        path = CostedPath()
+        path.poses = [[origin[0], origin[1], 0.0]]
+        path.cost = 0
+        results = []
+        costs = []
+
+        res = 1.0
+        for angle in np.linspace(-MAX_TURN_ANGLE, MAX_TURN_ANGLE, branches):
+            self.generatePaths(depth-1, path, angle, segment_length,
+                               res, branches, results, costs, costmap)
+
+        # results = np.asarray(results)
+        # print(results)
+        # plt.imshow(costmap, origin='lower')
+        # plt.xlim((25,75))
+        # plt.ylim((60,90))
+        return results
 
     def costMapCb(self, msg: OccupancyGrid):
-        if self.ego_pose is None:
-            print("Ego pose is None")
-            return
-        grid = np.asarray(msg.data, dtype=np.int8).reshape(
+        start = time.time()
+        costmap = np.asarray(msg.data, dtype=np.int8).reshape(
             msg.info.height, msg.info.width)
+        results = self.startGeneration(
+            costmap, depth=4, segment_length=12, branches=9)
 
-        origin_path = CostedPath()
-        hdg = self.ego_pose[2]
-        heading_vector = np.asarray([np.cos(hdg), np.sin(hdg)])
-        paths = self.getPaths(grid, self.ego_pose, origin_path,
-                              end_vec=heading_vector, depth=DEPTH)
-        return
+        min_cost = 100000
+        best_path: CostedPath = None
+
+        for result in results:
+            if result.cost < min_cost:
+                min_cost = result.cost
+                best_path = result
+
+        result_msg = Path()
+        result_msg.header.frame_id = "base_link"
+        result_msg.header.stamp = self.clock.clock
+
+        if best_path is None:
+            self.get_logger().error("No valid path was found!")
+            return
+
+        for pose in best_path.poses:
+            pose_msg = PoseStamped()
+            pose_msg.header.frame_id = "base_link"
+            pose_msg.header.stamp = self.clock.clock
+            pose_msg.pose.position.x = pose[0] * 0.4 - 20
+            pose_msg.pose.position.y = pose[1] * 0.4 - 30
+            # TODO: Add heading (pose[2]?)
+            result_msg.poses.append(pose_msg)
+
+        self.path_pub.publish(result_msg)
+
+        print(f"Done in {time.time() - start}!")
 
     def odomCb(self, msg: Odometry):
 
@@ -125,8 +222,6 @@ class RecursiveTreePlanner(Node):
         heading = np.arcsin(msg.pose.pose.orientation.z) * 2
 
         self.ego_pose = [pos.x, pos.y, heading]
-
-        print(f"Got odom: {self.ego_pose}")
         return
 
 
