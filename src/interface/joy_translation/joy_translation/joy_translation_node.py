09c4442d70fe8b9546a10ee4c87746dc897171df
Will Heitman <will.heitman@utdallas.edu>
2023-03-22 18:34:21 -0500

    Begin joy_translation refactor

--- src/interface/joy_translation/joy_translation/joy_translation_node.py
+++ src/interface/joy_translation/joy_translation/joy_translation_node.py
@@ -1,35 +1,68 @@
-import numpy as np
-import pickle
-import sys
-import os
-import math
-from rosgraph_msgs.msg import Clock
+'''
+Package:   joy_translation
+Filename:  joy_translation_node.py
+Author:    Ashwin Somasundaram, Will Heitman (w at heit.mn)
 
+Converts Joy messages to vehicle control commands
+
+Xbox One controller axes:
+[0]: LS, X
+[1]: LS, Y
+[2]: LT
+[3]: LS, X
+[4]: LS, Y
+[5]: RT
+[6]: Dpad X
+[7]: Dpad Y
+
+Xbox One controller buttons:
+[0]: A
+[1]: B
+[2]: X
+[3]: Y
+[4]: LB
+[5]: RB
+[6]: Select (left)
+[7]: Start (right)
+[8]: Xbox
+[9]: LSB
+[10]: RSB
+
+Controls:
+A: Auto enable (hold)
+X: Manual enable (hold, takes precedence over A)
+LS: Steering position
+LT: Brake
+RT: Throttle
+
+Subscribes to:
+ /joy (sensor_msgs/Joy)
+
+Publishes to:
+/carla/hero/vehicle_control_cmd (CarlaEgoVehicleControl)
+'''
+
+import numpy as np
+from rosgraph_msgs.msg import Clock
 
 from carla_msgs.msg import CarlaEgoVehicleControl
 import rclpy
 from rclpy.node import Node
-import ros2_numpy as rnp
-from sensor_msgs.msg import PointCloud2
 from sensor_msgs.msg import Joy
-from std_msgs.msg import String
-from nav_msgs.msg import OccupancyGrid, MapMetaData
-from nova_msgs.msg import StaticGrid
-from nova_msgs.msg import GridRow
-
 
 
 class joy_translation_node(Node):
-    target_joy = 0.0
-    output_msg = None
+    joy_sub = 0.0
+    command_pub = None
 
     def __init__(self):
         super().__init__('joy_translation_node')
-        self.target_joy = self.create_subscription(Joy, '/joy',self.process_joy, 10)
-        self.output_msg = self.create_publisher(CarlaEgoVehicleControl, '/carla/hero/vehicle_control_cmd', 10)
-        #self. = self.create_publisher(array, 'static_grid', 10)
+        self.joy_sub = self.create_subscription(
+            Joy, '/joy', self.joyCb, 10)
+        self.command_pub = self.create_publisher(
+            CarlaEgoVehicleControl, '/carla/hero/vehicle_control_cmd', 10)
 
-    def process_joy(self, joy_msg: Joy):
+    def joyCb(self, joy_msg: Joy):
         msg = CarlaEgoVehicleControl()
         msg.header.stamp = Clock().clock
         msg.header.frame_id = 'base_link'
@@ -41,17 +74,20 @@ class joy_translation_node(Node):
 
         msg.steer = joy_msg.axes[0]*-1
         msg.brake = joy_msg.axes[2]*-1
-        msg.hand_brake = True if joy_msg.buttons[2]==1 else False
-        msg.reverse = True if joy_msg.buttons[0]==1 else False
-        msg.gear = True if joy_msg.buttons[3]==1 else False
-        msg.manual_gear_shift = True if joy_msg.buttons[1]==1 else False
-        self.output_msg.publish(msg)
+
+        # Let's leave these out for now, as Navigator does not support them.
+        # msg.hand_brake = True if joy_msg.buttons[2] == 1 else False
+        # msg.reverse = True if joy_msg.buttons[0] == 1 else False
+        # msg.gear = True if joy_msg.buttons[3] == 1 else False
+        # msg.manual_gear_shift = True if joy_msg.buttons[1] == 1 else False
+
+        self.command_pub.publish(msg)
 
 
 def main(args=None):
     rclpy.init(args=args)
     JOYNODE = joy_translation_node()
     rclpy.spin(JOYNODE)
-    #self.get_logger().info('#####################################################################################')
+    # self.get_logger().info('#####################################################################################')
     joy_translation_node.destroy_node()
     rclpy.shutdown()
