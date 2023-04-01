052ed8fae188ce1ec385f3a46adfb63f2f8ef250
Will Heitman <Will.Heitman@UTDallas.edu>
2023-04-01 13:38:22 -0500

    Feature linear actuators (#349)
    * Add linear actuator docs
    
    * Apply .gitignore to docs/_site
    
    * Re-add Guardian data
    
    * Add Clock package
    
    * Add to guardian
    
    * Add EPAS code to main.launch
    
    * Finish LA code

--- src/interface/joy_translation/joy_translation/joy_translation_node.py
+++ src/interface/joy_translation/joy_translation/joy_translation_node.py
@@ -47,6 +47,7 @@ from rosgraph_msgs.msg import Clock
 import time
 
 from carla_msgs.msg import CarlaEgoVehicleControl
+from diagnostic_msgs.msg import DiagnosticStatus, KeyValue
 from nova_msgs.msg import Mode
 import rclpy
 from rclpy.node import Node
@@ -61,31 +62,90 @@ class joy_translation_node(Node):
         self.joy_sub = self.create_subscription(
             Joy, '/joy', self.joyCb, 10)
 
-    def process_joy(self, joy_msg: Joy):
-        #self.get_logger().info('bruh')
-        msg = CarlaEgoVehicleControl()
-        msg.header.stamp = Clock().clock
-        msg.header.frame_id = 'base_link'
-        msg.throttle = ((joy_msg.axes[5]*-1)+1)/2
+        self.clock = Clock().clock
+        self.clock_sub = self.create_subscription(
+            Clock, '/clock', self.clockCb, 10)
 
-        # TODO: Fix this jank.
-        if command_msg.throttle == 0.5:
-            command_msg.throttle = 0.0
+        self.command_pub = self.create_publisher(
+            CarlaEgoVehicleControl, '/carla/hero/vehicle_control_cmd', 10)
 
-        msg.steer = joy_msg.axes[0]*-1
+        self.requested_mode_pub = self.create_publisher(
+            Mode, '/requested_mode', 1)
         
-        if abs(msg.steer)<0.2:
-            msg.steer = 0.0
-        #self.get_logger().info(str(msg.steer))
-        msg.brake = joy_msg.axes[2]
-        
-        #self.get_logger().info('this is the val of brake sent in')
-        #self.get_logger().info(str(msg.brake))
-        msg.hand_brake = True if joy_msg.buttons[2]==1 else False
-        msg.reverse = True if joy_msg.buttons[0]==1 else False
-        msg.gear = True if joy_msg.buttons[3]==1 else False
-        msg.manual_gear_shift = True if joy_msg.buttons[1]==1 else False
-        self.output_msg.publish(msg)
+        self.status = DiagnosticStatus()
+        self.status_pub = self.create_publisher(DiagnosticStatus, '/node_statuses', 1)
+
+        self.current_mode = Mode.DISABLED
+        self.current_mode_sub = self.create_subscription(
+            Mode, '/guardian/mode', self.currentModeCb, 1)
+
+    def currentModeCb(self, msg: Mode):
+        self.current_mode = msg.mode
+
+    def clockCb(self, msg: Clock):
+        self.clock = msg.clock
+
+    def initStatusMsg(self) -> DiagnosticStatus:
+        status = DiagnosticStatus()
+
+        status.name = self.get_name()
+
+        stamp = KeyValue()
+        stamp.key = 'stamp'
+        stamp.value = str(self.clock.sec+self.clock.nanosec*1e-9)
+        status.values.append(stamp)
+
+        status.level = DiagnosticStatus.OK
+
+        return status
+
+    def joyCb(self, msg: Joy):
+        command_msg = CarlaEgoVehicleControl()
+
+        self.status = self.initStatusMsg()
+
+        left_trigger = msg.axes[2]
+        # if left_trigger == 0.:
+        #     left_trigger = -1.
+        right_trigger = msg.axes[5]
+        if right_trigger == 0.:
+            right_trigger = -1.
+
+        command_msg.header.stamp = self.clock
+        command_msg.header.frame_id = 'base_link'
+        command_msg.throttle = ((right_trigger*-1)+1)/2
+        command_msg.steer = msg.axes[0]*-1
+
+        if left_trigger == 0.:
+            command_msg.brake = 0.
+        else:
+            command_msg.brake = 1-(left_trigger+1)/2
+
+
+        requested_mode = Mode()
+        if msg.buttons[2] == 1:
+            requested_mode.mode = Mode.MANUAL
+        elif msg.buttons[0] == 1:
+            requested_mode.mode = Mode.AUTO
+        else:
+            requested_mode.mode = Mode.DISABLED
+
+        # Let's leave these out for now, as Navigator does not support them.
+        # msg.hand_brake = True if joy_msg.buttons[2] == 1 else False
+        # msg.reverse = True if joy_msg.buttons[0] == 1 else False
+        # msg.gear = True if joy_msg.buttons[3] == 1 else False
+        # msg.manual_gear_shift = True if joy_msg.buttons[1] == 1 else False
+
+        if self.current_mode == Mode.MANUAL:
+            self.command_pub.publish(command_msg)
+            # self.get_logger().info("Publishing manual command!")
+        self.requested_mode_pub.publish(requested_mode)
+
+        requested_mode_keyval = KeyValue()
+        requested_mode_keyval.key = 'requested_mode'
+        requested_mode_keyval.value = str(requested_mode)
+        self.status.values.append(requested_mode_keyval)
+        self.status_pub.publish(self.status)
 
 
 def main(args=None):
