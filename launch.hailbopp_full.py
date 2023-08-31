4b13cc8dc32fbe7ea0ee13932b5f483f3f4d3dec
Justin Ruths <jruths@gmail.com>
2023-08-31 10:28:29 -0500

    consolidated launch file structure. got radar integrated into rviz

---
+++ launch.hailbopp_full.py
@@ -0,0 +1,62 @@
+from os import name, path, environ
+
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import ExecuteProcess
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+from ament_index_python import get_package_share_directory
+
+sys.path.append(path.abspath('/navigator/'))
+from launch_node_definitions import *
+
+NAVIGATOR_DIR = "/navigator/"
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        # CONTROL
+        # controller,
+
+        # INTERFACE
+        # camera_streamer,
+        joy,
+        joy_translator,
+        epas,
+        mcu_interface,
+        linear_actuator,
+        # web_bridge,
+        gnss,
+        left_lidar_driver,
+        left_lidar_pointcloud,
+        right_lidar_driver,
+        right_lidar_pointcloud,
+        # camera,
+
+        # MISC
+        clock,
+        
+        # recorder,
+        urdf_publisher,
+        # rviz,
+
+        # PERCEPTION
+        ground_seg,
+        lidar_processor,
+        static_grid,
+        
+        # PLANNING
+        map_manager,
+        grid_summation,
+        rtp,
+        odom2tf,
+        junction_manager,
+
+        # SAFETY
+        guardian,
+        sounds,
+    ])
