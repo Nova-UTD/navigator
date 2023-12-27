7023b15c4f8ab81ab8053de1d65ec51aab5d8b0c
Kevin Ge <ge.kevin.a@gmail.com>
2023-12-27 11:06:14 -0600

    refactor: move launch files to `launches`  directory and nest perception and vehicle stacks

--- launches/launch_node_definitions.py
+++ launches/launch_node_definitions.py
@@ -1,15 +1,6 @@
-from os import name, path, environ
+"""This module defines nodes used in launch files."""
 
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import IncludeLaunchDescription
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import ExecuteProcess
-from launch.conditions import IfCondition
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-
-from ament_index_python import get_package_share_directory
 
 NAVIGATOR_DIR = "/navigator/"
 
