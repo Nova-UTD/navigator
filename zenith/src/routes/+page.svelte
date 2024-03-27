8f8861330430d5603713ea3c26e5e785c39b1be9
Kevin Ge <ge.kevin.a@gmail.com>
2024-03-27 13:21:29 -0500

    feat: move launch list into separate component

--- zenith/src/routes/+page.svelte
+++ zenith/src/routes/+page.svelte
@@ -1,42 +1,14 @@
 <script lang="ts">
-	import { LaunchList, LaunchListSkeleton, LaunchCreateDialog } from '$lib/modules/launch-list';
 	import { LaunchEditor } from '$lib/modules/launch-editor';
-	import { Button } from '$lib/ui/button';
-	import PlusSquare from 'lucide-svelte/icons/plus-square';
-	import {
-		launchStore,
-		selectedLaunchStore,
-		addNodes,
-		removeNodes,
-		setSelectedLaunchIndex
-	} from '$lib/stores/launchStore';
+	import { launchStore, selectedLaunchStore, addNodes, removeNodes } from '$lib/stores/launchStore';
 	import { subsystemStore } from '$lib/stores/subsystemStore';
 	import { loadAll } from '@square/svelte-store';
 	import { LaunchTerminal } from '$lib/modules/launch-terminal';
+	import { LaunchListPanel } from '$lib/modules/launch-list';
 </script>
 
 <section class="w-full h-full overflow-hidden grid grid-cols-[0.5fr_1fr_1fr] gap-[5%]">
-	<section class="flex flex-col gap-5">
-		<article class="flex justify-between items-center">
-			<h1>Launch List</h1>
-			<LaunchCreateDialog>
-				<Button variant="ghost"><PlusSquare /></Button>
-			</LaunchCreateDialog>
-		</article>
-		{#await launchStore.load()}
-			<LaunchListSkeleton />
-			<p>Loading...</p>
-		{:then}
-			<LaunchList
-				onLaunchSelect={setSelectedLaunchIndex}
-				launches={$launchStore.launches.map((launch, index) => ({
-					name: launch.metadata.name,
-					path: launch.path,
-					selected: $launchStore.selectedLaunchIndex === index
-				}))}
-			/>
-		{/await}
-	</section>
+	<LaunchListPanel />
 	{#await loadAll([launchStore, subsystemStore])}
 		<p>Loading...</p>
 	{:then}
