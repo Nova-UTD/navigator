773055cd3c1e05ebe39b348e25975904ac6b4aec
Kevin Ge <ge.kevin.a@gmail.com>
2024-03-27 11:53:02 -0500

    fix: invalid generated launch file imports

--- zenith/server/launch/template.py
+++ zenith/server/launch/template.py
@@ -1,11 +1,15 @@
 # Base launch file used for launch file creation.
 # We currently assume launch_node_definitions.py will exist in the same directory as our launch file.
 LAUNCH_FILE_TEMPLATE = """
+import sys
+from os import path
+
 from launch import LaunchDescription
 from launch.actions import RegisterEventHandler, LogInfo
 from launch.event_handlers import OnProcessStart
 from launch_ros.actions import Node
 
+sys.path.append(path.abspath('/navigator/'))
 from launches.launch_node_definitions import *
 
 def generate_launch_description():
