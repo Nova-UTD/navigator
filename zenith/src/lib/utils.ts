4de68e79349d08a9dd01d1c6abee1777e0532171
Kevin Ge <ge.kevin.a@gmail.com>
2024-04-02 09:06:13 -0500

    docs: util functions

--- zenith/src/lib/utils.ts
+++ zenith/src/lib/utils.ts
@@ -5,29 +5,63 @@ import type { TransitionConfig } from 'svelte/transition';
 import type { Node } from './api';
 import { invoke } from '@tauri-apps/api';
 
+/**
+ * @param name - The name of the environment variable
+ * @param defaultValue - The default value to return if the environment variable is not set
+ * @returns - Returns the value of the environment variable or the default value if it is not set
+ * @example
+ * get_env('HOME') // '/home/user'
+ */
 export async function get_env(name: string, defaultValue: string = ''): Promise<string> {
 	return await invoke('get_env', { name, defaultValue });
 }
 
+/**
+ * @param node - The node to compare
+ * @param otherNode  - The other node to compare
+ * @returns  - Returns true if the nodes are equal, false otherwise
+ * @example
+ * isNodeEqual({ package: 'p1', executable: 'e1' }, { package: 'p1', executable: 'e1' }) // true
+ */
 export function isNodeEqual(node: Node, otherNode: Node): boolean {
 	return node.package === otherNode.package && node.executable === otherNode.executable;
 }
 
+/*
+ * @param array - The array to chunk
+ * @param size - The size of each chunk
+ * @returns - Returns a new array with the chunks
+ */
 export function chunkArray<T>(array: T[], size: number): T[][] {
 	return Array.from({ length: Math.ceil(array.length / size) }, (_, i) =>
 		array.slice(i * size, i * size + size)
 	);
 }
 
+/**
+ * @param path - The path to get the filename from
+ * @returns - Returns the filename from the path
+ * @example
+ * filenameFromPath('/home/user/file.txt') // 'file.txt'
+ */
 export function filenameFromPath(path: string): string {
 	return path.replace(/^.*[\\/]/, '');
 }
 
-// Generate sequential IDs.
+/**
+ * @returns - Returns a new time-sequential ID
+ */
 export function seqID(): string {
 	return (Date.now() + 1).toString(36);
 }
 
+/**
+ * @param inputs - The class values to merge
+ * @returns - Returns the merged class values with resolved conflicting classes
+ * @example
+ * cn('text-sm', 'text-lg') // 'text-lg'
+ * cn('text-lg', 'text-sm') // 'text-sm'
+ */
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
