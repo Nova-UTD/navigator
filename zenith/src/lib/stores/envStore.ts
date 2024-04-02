20ebb121720bf4aad69e0cbd603606e4671294b1
Kevin Ge <ge.kevin.a@gmail.com>
2024-04-02 09:18:15 -0500

    docs: svelte stores and README

--- zenith/src/lib/stores/envStore.ts
+++ zenith/src/lib/stores/envStore.ts
@@ -1,3 +1,5 @@
+// Wrapper for environment variables fetched from Tauri backend.
+
 import { get_env } from '$lib/utils';
 import { asyncReadable, type Loadable } from '@square/svelte-store';
 
@@ -9,6 +11,9 @@ const defaultEnv: EnvStore = {
 	launchDir: '../launches'
 };
 
+/**
+ * Store for environment variables fetched from Tauri (Rust) backend.
+ */
 export const envStore: Loadable<EnvStore> = asyncReadable(defaultEnv, async () => {
 	const launchDir = await get_env('LAUNCH_DIR', '../launches');
 	return { launchDir };
