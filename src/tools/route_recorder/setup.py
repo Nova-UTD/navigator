5f531a4743a566b4e6435aa86bfc6b037d0f0679
Daniel Vayman <daniel@vayman.co>
2024-04-27 13:39:06 -0500

    feat: add route recorder and lot u route

---
+++ src/tools/route_recorder/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'route_recorder'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='root@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'route_recorder_node = route_recorder.route_recorder_node:main'
+        ],
+    },
+)
