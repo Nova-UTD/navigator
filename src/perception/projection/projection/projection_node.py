a12a56a5b9c8f4f7721acde6e3826453aecc2023
David Homiller <dth210000@utdallas.edu>
2025-03-01 14:59:18 -0600

    final commit

--- src/perception/projection/projection/projection_node.py
+++ src/perception/projection/projection/projection_node.py
@@ -41,30 +41,13 @@ class ProjectionNode(Node):
     def __init__(self):
         super().__init__("projection_node")
         
-        # Camera parameters. Currently set for CARLA
-        fov = 70 # degrees
-        img_width, img_height = 800, 600 # pixels
-        c = np.array([0.7,-0.15,1.88]) # location of camera in CARLA coords
-        r = np.eye(3) # rotation of camera
+        self.declare_parameter('seg_topic', '/semantics/semantic0')
+        self.declare_parameter('camera_info_topic', '/carla/hero/rgb_center/camera_info')
 
-        # intrinsic matrix
-        f = img_width / (2 * math.tan(fov*math.pi/360)) # focal length
-        cu, cv = img_width/2, img_height/2 # center of image in pixel coordinates
-        m1 = np.array([[f, 0, cu, 0],
-                        [0, f, cv, 0],
-                        [0, 0, 1, 0]]) 
-        
-        # extrinsic matrix
-        t = - r@c
-        m2 = np.hstack((r, t.reshape(-1, 1)))
-        m2 = np.vstack((m2, [0,0,0,1]))
-
-        # final matrix
-        self.matrix = m1@m2
 
         # For testing. Remove later.
         self.publisher_ = self.create_publisher(String, '/terror', 10)
-        self.timer = self.create_timer(5, self.timer_cb)
+        self.timer = self.create_timer(0.02, self.timer_cb) # should i use a timer? what should the period be?
         self.bridge = CvBridge()
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
@@ -78,12 +61,21 @@ class ProjectionNode(Node):
         self.lidar_sub = self.create_subscription(
             PointCloud2, '/lidar', self.lidar_callback, 10)
         # Subscribes to camera
+        # self.camera_sub = self.create_subscription(
+        #     Image, '/cameras/camera0', self.camera_callback, 10)
         self.camera_sub = self.create_subscription(
-            Image, '/cameras/camera0', self.camera_callback, 10)
+            Image, '/semanticsample', self.camera_callback, 10)
 
         # Subscribes to camera info
         camera_info_sub = self.create_subscription(
             CameraInfo, '/carla/hero/rgb_center/camera_info', self.camera_info_cb, 10)
+        
+        # Test code to subscribe to semantic segmentation, then convert to simpler classes
+        segmentation_sub = self.create_subscription(
+            Image, '/semantics/semantic0', self.segmentation_cb, 10)
+        self.seg_pub = self.create_publisher(
+            Image, '/semanticsample', 10)
+        
         # Subscribes to clock for headers
         self.clock_sub = self.create_subscription(
             Clock, '/clock', self.clock_cb, 10)
@@ -95,7 +87,6 @@ class ProjectionNode(Node):
         self.cam_arr = None
         self.cam_model = None
 
-    # For testing. Remove later.
     def timer_cb(self):
         pass
 
@@ -123,6 +114,7 @@ class ProjectionNode(Node):
         self.cam_model.fromCameraInfo(msg)
 
     def camera_callback(self, camera_msg: Image):
+        pass
         if self.cam_arr is None:
             return
         if self.cam_model is None:
@@ -136,20 +128,20 @@ class ProjectionNode(Node):
         P = np.asarray(self.cam_model.projectionMatrix())
         cam_arr_1 = np.hstack((self.cam_arr, np.ones((len(self.cam_arr),1))))
         pix_arr = (P @ cam_arr_1.T).T
-        pix_arr[:,0:2] = (pix_arr[:,0:2].T / pix_arr[:,2]).T
+        pix_arr[:,0:2] = (pix_arr[:,0:2].T / pix_arr[:,2]).T #.astype(int)
         #pix_arr = pix_arr.astype(int)
-        # self.get_logger().info('hi %s' % np.array2string(pix_arr))
+        #self.get_logger().info('hi %s' % np.array2string(pix_arr))
         for pt in pix_arr:
             #uv = self.cam_model.project3dToPixel(pt)
-            if pt[0] > 1023 or pt[0] < 0:
-                continue
-            elif pt[1] > 511 or pt[1] < 0:
-                continue
+            # if pt[0] > 1023 or pt[0] < 0:
+            #     continue
+            # elif pt[1] > 511 or pt[1] < 0:
+            #     continue
             color = max((int(255-5*pt[2]), 0))
             cv2.circle(cv_image, (int(pt[0]),int(pt[1])), 3, (color,0,color), -1)
 
-        cv2.imshow("Image window", cv_image)
-        cv2.waitKey(3)
+        # cv2.imshow("Image window", cv_image)
+        # cv2.waitKey(3)
 
         # Project LIDAR points onto image coordinates
         
@@ -162,7 +154,9 @@ class ProjectionNode(Node):
         # cam_arr = cam_arr[cam_arr[:, 1] < 600] 
 
         self.image = camera_msg
-        
+
+    def get_color(x):
+        return self.cv_image[x[1],x[0]]
 
     def lidar_callback(self, lidar_msg: PointCloud2):
         """! 
@@ -197,10 +191,54 @@ class ProjectionNode(Node):
                              t.transform.translation.z]
 
         # Only keep points in front of the camera
-        cam_arr = cam_arr[cam_arr[:, 2] > 0]                      
+        cam_arr = cam_arr[cam_arr[:, 2] > 0]    
+
+        # add color
+        
+            
+        rgb = np.array(rgb).astype(np.uint32)
+
+        if len(classified_pts) < 1:
+            return
+
+        xyzc = np.zeros(classified_pts.shape[0], dtype=[
+            ('x', np.float32),
+            ('y', np.float32),
+            ('z', np.float32),
+            ('rgb', np.uint32)
+        ])
+
+        xyzc['x'] = classified_pts[:, 0]
+        xyzc['y'] = classified_pts[:, 1]
+        xyzc['z'] = classified_pts[:, 2]
+        xyzc['rgb'] = rgb                  
                              
         self.cam_arr = cam_arr
-        
+
+    
+
+    def segmentation_cb(self, camera_msg: Image):
+        if self.cam_arr is None:
+            return
+        if self.cam_model is None:
+            return
+
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(camera_msg, "bgr8")
+        except CvBridgeError as e:
+            print(e)        
+
+        for y in range(cv_image.shape[0]):
+            for x in range(cv_image.shape[1]):
+                if cv_image[y, x, 0] < 100:
+                    cv_image[y, x] = [0, 0, 0]  # Set dark pixels to black
+                else:
+                    cv_image[y,x] = [255,255,255] # Set bright pixels to white
+        self.cv_image = cv_image
+
+        cv2.imshow("Image window", cv_image)
+        cv2.waitKey(3)
+
 def main(args=None):
     rclpy.init(args=args)
 
