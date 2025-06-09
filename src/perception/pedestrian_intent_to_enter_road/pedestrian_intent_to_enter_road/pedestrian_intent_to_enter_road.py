247993033dbc7997bd017a0c27c3308765c6f79f
Pranav Boyapati <pkb230000@utdallas.edu>
2025-06-09 12:53:55 -0500

    Detection of all people, start of detecting direction facing, and overall structure for node

--- src/perception/pedestrian_intent_to_enter_road/pedestrian_intent_to_enter_road/pedestrian_intent_to_enter_road.py
+++ src/perception/pedestrian_intent_to_enter_road/pedestrian_intent_to_enter_road/pedestrian_intent_to_enter_road.py
@@ -4,73 +4,99 @@ File: pedestrian_intent_to_enter_road.py
 Author: Pranav Boyapati
 """
 
+# import libraries
 import rclpy
-import base64
-import os
 from rclpy.node import Node
 from sensor_msgs.msg import Image
+from navigator_msgs.msg import PedestrianInfoDetections
+from navigator_msgs.msg import PedestrianInfo
 from cv_bridge import CvBridge
-from navigator_msgs.msg import RoadSigns
-from navigator_msgs.msg import RoadSignsDetection
 import cv2
-from inference_sdk import InferenceHTTPClient
+from ultralytics import YOLO
 
-
-class RoadSignsClassifier(Node):
+class PedestrianIntentToEnterRoad(Node):
     def __init__(self):
-        super().__init__('road_signs_classifier')
+        super().__init__('pedestrian_intent_to_enter_road')
 
-        #instantiate model client
-        self.CLIENT = InferenceHTTPClient(api_url="http://localhost:6060", api_key="BmqYjCBXZD1iPIyq09sG")
+        # create subscriber and publisher
+        self.camera_subscription = self.create_subscription(Image, '/cameras/camera0', self.image_callback, 10)
+        self.pedestrian_publisher = self.create_publisher(PedestrianInfoDetections, '/pedestrians', 10)
 
-        #create subscriptions
-        self.camera_sub = self.create_subscription(Image, '/cameras/camera0', self.image_callback, 10)
-
-        #create variables to store subscription info
+        # define variables used throughout the node
         self.bridge = CvBridge()
         self.image = None
-
-        #create publisher
-        self.road_signs_publisher = self.create_publisher(RoadSignsDetection, '/road_signs/detections', 10)
+        self.detection_model = YOLO("/navigator/data/perception/models/pedestrian_detection_model.pt")
 
 
-    #callbacks for subscriptions
     def image_callback(self, msg : Image):
+        # convert the image message into a jpg formatted image
         cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough") 
-        status, self.image = cv2.imencode(".jpeg", cv_image) 
-        self.classify_sign()
+        status, temp_image = cv2.imencode(".jpg", cv_image) 
+        self.image = cv2.imdecode(temp_image, cv2.IMREAD_COLOR)
 
-    
-    #main control function
-    def classify_sign(self):
-        #gets prediction
-        img_base64 = base64.b64encode(self.image).decode('utf-8')
-        result = self.CLIENT.infer(img_base64, model_id="us-road-signs/71")
+        # call the detect_pedestrians function, which performs all of the detections and filtering for this node
+        self.detect_pedestrians()
 
-        #create message and publish
-        if (len(result["predictions"]) > 0):
 
-            road_signs_detection_msg = RoadSignsDetection()
-            road_signs_detection_msg.header.stamp = self.get_clock().now().to_msg()
-            road_signs_detection_msg.header.frame_id = "camera_frame"
+    def detect_pedestrians(self):
+        # perform object detection for pedestrians on the input image
+        results = self.detection_model(self.image)
+
+        # instantiate an array for holding all detections
+        pedestrian_detections = []
+
+        # this executes for each pedestrian detected by the model
+        for result in results:
+            # get the location and dimensions of the pedestrian bounding box
+            xywh = result.boxes.xywh
+            center_x, center_y, width, height = xywh
+
+            # TODO: use MMPose to detect whether the pedestrian is facing sideways
+            xyxy = result.boxes.xyxy
+            TLx, TLy, BRx, BRy = xyxy
+
+            image_to_crop = cv2.imread(self.image)
+            cropped_img = image_to_crop[TLy:BRy, TLx:BRx].copy()
+            image_of_person = cv2.imdecode(cropped_img, cv2.IMREAD_COLOR)
+
+            # NOTE: the full image has been cropped to only have the person of interest. Now, use this image to determine direction person is facing
             
-            road_sign = RoadSigns()
-            road_sign.x = result["predictions"][0]['x']
-            road_sign.y = result["predictions"][0]['y']
-            road_sign.width = result["predictions"][0]['width']
-            road_sign.height = result["predictions"][0]['height']
-            road_sign.label = result["predictions"][0]['class']
-            road_sign.confidence = result["predictions"][0]['confidence']
 
-            road_signs_detection_msg.road_signs.append(road_sign)
+            # TODO: if the pedestrian is facing sideways, determine if that is towards or away from the road
+            # NOTE: For this, determine which half of the image the pedestrian is on. Left half facing right and right half facing left is considered towards road
 
-            self.road_signs_publisher.publish(road_signs_detection_msg)
+            # for pedestrians facing the road, determine the distance between them and the road
+            horizontal_dist = self.calculate_horizontal_distance()
+
+            # create a PedestrianInfo message object for the pedestrian and append to the detections array
+            pedestrian_object = PedestrianInfo()
+
+            pedestrian_object.x = center_x
+            pedestrian_object.y = center_y
+            pedestrian_object.width = width
+            pedestrian_object.height = height
+            pedestrian_object.distance = horizontal_dist
+
+            pedestrian_detections.append(pedestrian_object)
+
+        # create and publish a PedestrianInfoDetections message
+        detections_msg = PedestrianInfoDetections()
+
+        detections_msg.header.stamp = self.get_clock().now().to_msg()
+        detections_msg.header.frame_id = "camera_frame"
+        detections_msg.pedestrians = pedestrian_detections
+
+        self.pedestrian_publisher.publish(detections_msg)
+
+
+    def calculate_horizontal_distance(self):
+        # TODO: determine the horizontal distance between the pedestrian and the edge of the road
 
 
 def main(args=None):
     rclpy.init(args=args)
-    road_signs_classification = RoadSignsClassifier()
-    rclpy.spin(road_signs_classification)
+    pedestrian_intent_to_enter_road = PedestrianIntentToEnterRoad()
+    rclpy.spin(pedestrian_intent_to_enter_road)
     rclpy.shutdown()
 
 
