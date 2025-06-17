ab1dad16eb8960e1c4204bee45ccc1f06ad8db93
Pranav Boyapati <pkb230000@utdallas.edu>
2025-06-16 23:27:48 -0500

    Function for calculating distance

--- src/perception/pedestrian_intent_to_enter_road/pedestrian_intent_to_enter_road/pedestrian_intent_to_enter_road.py
+++ src/perception/pedestrian_intent_to_enter_road/pedestrian_intent_to_enter_road/pedestrian_intent_to_enter_road.py
@@ -21,6 +21,7 @@ class PedestrianIntentToEnterRoad(Node):
 
         # create subscriber and publisher
         self.camera_subscription = self.create_subscription(Image, '/cameras/camera0', self.image_callback, 10)
+        self.binary_mask_subscription = self.create_subscription(Image, '/segmentation_mask', self.binary_mask_callback, 10)
         self.pedestrian_publisher = self.create_publisher(PedestrianInfoDetections, '/pedestrians', 10)
 
         # define variables used throughout the node
@@ -28,6 +29,10 @@ class PedestrianIntentToEnterRoad(Node):
         self.image = None
         self.detection_model = YOLO("/navigator/data/perception/models/pedestrian_detection_model.pt")
         self.inferencer = MMPoseInferencer('human')
+        self.binary_mask = None
+
+        FACING_RIGHT = "RIGHT"
+        FACING_LEFT = "LEFT"
 
 
     def image_callback(self, msg : Image):
@@ -38,6 +43,13 @@ class PedestrianIntentToEnterRoad(Node):
 
         # call the detect_pedestrians function, which performs all of the detections and filtering for this node
         self.detect_pedestrians()
+
+
+    def binary_mask_callback(self, msg : Image):
+        # convert the image message into a jpg formatted image
+        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough") 
+        status, temp_image = cv2.imencode(".jpg", cv_image) 
+        self.binary_mask = cv2.imdecode(temp_image, cv2.IMREAD_COLOR)
 
 
     def detect_pedestrians(self):
@@ -70,18 +82,18 @@ class PedestrianIntentToEnterRoad(Node):
             right_eye_keypoint = pose_estimation_result["predictions"][0][0]["keypoints"][2]
 
             if ((nose_keypoint[0] > left_eye_keypoint[0]) and (nose_keypoint[0] > right_eye_keypoint[0])):
-                direction_facing = "RIGHT"
+                direction_facing = FACING_RIGHT
             elif ((nose_keypoint[0] < left_eye_keypoint[0]) and (nose_keypoint[0] < right_eye_keypoint[0])):
-                direction_facing = "LEFT"
+                direction_facing = FACING_LEFT
             else:
                 continue
 
             height, width, channels = self.image.shape
             midpoint_x = width / 2
 
-            if (((direction_facing == "RIGHT") and (center_x < midpoint_x)) or ((direction_facing == "LEFT") and (center_x > midpoint_x))):
+            if (((direction_facing == FACING_RIGHT) and (center_x < midpoint_x)) or ((direction_facing == FACING_LEFT) and (center_x > midpoint_x))):
                 # for pedestrians facing the road, determine the distance between them and the road
-                horizontal_dist = self.calculate_horizontal_distance()
+                horizontal_dist = self.calculate_horizontal_distance(bounding_box_coordinates=xyxy, direction_facing=direction_facing)
 
                 # create a PedestrianInfo message object for the pedestrian and append to the detections array
                 pedestrian_object = PedestrianInfo()
@@ -106,8 +118,36 @@ class PedestrianIntentToEnterRoad(Node):
         self.pedestrian_publisher.publish(detections_msg)
 
 
-    def calculate_horizontal_distance(self):
-        # TODO: determine the horizontal distance between the pedestrian and the edge of the road
+    def calculate_horizontal_distance(self, bounding_box_coordinates, direction_facing):
+        num_pixels = 0
+        TLx, TLy, BRx, BRy = bounding_box_coordinates
+
+        if (self.segmentation_mask is not None):
+            if (direction_facing == FACING_RIGHT):
+                while (True):
+                    pixel_color = self.binary_mask[BRx + num_pixels, BRy]
+
+                    if (pixel_color == [0, 0, 0]):
+                        break
+                    else:
+                        num_pixels += 1
+            elif (direction_facing == FACING_LEFT):
+                while (True):
+                    pixel_color = self.binary_mask[TLx - num_pixels, BRy]
+
+                    if (pixel_color == [0, 0, 0]):
+                        break
+                    else:
+                        num_pixels += 1
+
+            depth = 470 * 1.75 / (BRy - TLy)        # focal length * real height of person / height of person in pixels
+            total_width_at_person_depth = 2 * depth * 1.0913        # 2 * depth * tan(FoV / 2)
+            height, width, channels = self.image.shape
+            hor_dist = num_pixels / width * total_width_at_person_depth
+
+            return hor_dist
+        else:
+            return
 
 
 def main(args=None):
