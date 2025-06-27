c9cdcfcb9d67baffe94c488dd56ab6a7b53ee092
Pranav Boyapati <pkb230000@utdallas.edu>
2025-06-27 12:38:18 -0500

    Update package setup files

--- src/perception/pedestrian_skeleton/setup.py
+++ src/perception/pedestrian_skeleton/setup.py
@@ -4,28 +4,19 @@ package_name = 'pedestrian_skeleton'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=[
-        'setuptools',
-        'rclpy',
-        'torch'
-        'sensor_msgs',
-        'cv_bridge',
-        'std_msgs',
-        'opencv-python',
-        'ultralytics'
-    ],
+    install_requires=['setuptools',],
     zip_safe=True,
     maintainer='root',
-    maintainer_email='root@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer_email='project.nova@utdallas.edu',
+    description='YOLOv8-based pedestrian skeleton detection in ROS2',
+    license='MIT License',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
