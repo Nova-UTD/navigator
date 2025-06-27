9a810b2ce7777aaf066aaa38b223b37592473e43
Pranav Boyapati <pkb230000@utdallas.edu>
2025-06-27 12:02:38 -0500

    Update traffic_light_node.py (#460)
    * Update traffic_light_node.py
    
    Working
    
    * Delete unnecessary files
    
    * Finish package setup files
    
    * Move model to git lfs and update path
    
    * Add model using git lfs
    
    ---------
    
    Co-authored-by: Vindhya Kaushal <76739983+VINDHYAkaushal@users.noreply.github.com>

--- src/perception/traffic_light_detector/setup.py
+++ src/perception/traffic_light_detector/setup.py
@@ -13,10 +13,10 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='root@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Project Nova',
+    maintainer_email='project.nova@utdallas.edu',
+    description='Classifies traffic lights seen in camera images',
+    license='MIT License',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
