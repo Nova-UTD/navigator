5e1ccc3e079cfe8e78ddd25e9f9514503672e977
Pranav Boyapati <pkb230000@utdallas.edu>
2026-02-17 22:30:05 -0600

    Debug messages

--- src/planning/path_planners/path_planners/path_planner_node.py
+++ src/planning/path_planners/path_planners/path_planner_node.py
@@ -195,11 +195,11 @@ class PathPlannerNode(Node):
             self.get_logger().warning("Incoming cost map dimensions were zero.")
             return
         self.costmap = msg
-        self.get_logger().debug(f"Received costmap: {msg.info.width}x{msg.info.height}")
+        self.get_logger().info(f"Received costmap: {msg.info.width}x{msg.info.height}")
 
     def path_goal_callback(self, msg: PoseStamped):
         self.path_goal = msg
-        self.get_logger().debug(
+        self.get_logger().info(
             f"Received goal: ({msg.pose.position.x}, {msg.pose.position.y})"
         )
 
@@ -258,6 +258,7 @@ class PathPlannerNode(Node):
         # Prepare costmap data
         data_dim = (self.costmap.info.height / self.grid_res) * (self.costmap.info.width / self.grid_res)
         data_dim = int(data_dim)
+        self.get_logger().info(f"Reshaping costmap data to dimensions: {int(self.costmap.info.height / self.grid_res)}x{int(self.costmap.info.width / self.grid_res)} (total cells: {data_dim})")
 
         if (len(self.costmap.data) < data_dim):
             for i in range(data_dim - len(self.costmap.data)):
